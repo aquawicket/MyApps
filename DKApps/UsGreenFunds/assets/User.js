24e84675f5c0e2b2dd338e7c18dcc2c3f5643a91
aquawicket <aquawicket@hotmail.com>
2016-05-12 20:45:06 -0700

    update

--- DKApps/UsGreenFunds/assets/User.js
+++ DKApps/UsGreenFunds/assets/User.js
@@ -1,5 +1,6 @@
+serverpath = "http://usgreenfunds.com";
 DKCreate("DKJavascript,DKDev/DKDev.js");
 DKCreate("DKWidget,Home.html");
-DKDev_DevModeOff();
+//DKDev_DevModeOff();
 DKCreate("DKJavascript,DKTriggers/DKTriggers.js");
 DKTrigger_LoadTriggers("triggers.txt");
