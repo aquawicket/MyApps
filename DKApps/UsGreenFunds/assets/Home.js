51d071e76dabc908cdc3c9016cb1f47d2f2f29cd
aquawicket <aquawicket@hotmail.com>
2016-05-25 20:00:54 -0700

    update

--- DKApps/UsGreenFunds/assets/Home.js
+++ DKApps/UsGreenFunds/assets/Home.js
@@ -4,11 +4,14 @@ DKCreate("DKWidget,Home.html");
 function Home_Init()
 {
 	DKRegisterEvent("login", "click", Home_OnEvent);
+	DKRegisterEvent("GLOBAL", "keydown", Home_OnEvent);
 }
 
 ////////////////////////////
 function Home_OnEvent(event)
 {
+	//DKLog(event);
+	
 	var password = DKWidget_GetValue("password");
 	if(password == "million"){
 		DKLog("Yup \n");
