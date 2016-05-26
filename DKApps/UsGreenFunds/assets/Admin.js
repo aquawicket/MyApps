e73309e2a396aee11ad2578e0d20bca7d2126fce
aquawicket <aquawicket@hotmail.com>
2016-05-26 00:18:46 -0700

    update

--- DKApps/UsGreenFunds/assets/Admin.js
+++ DKApps/UsGreenFunds/assets/Admin.js
@@ -4,34 +4,22 @@ DKCreate("DKWidget,Admin.html");
 function Admin_Init()
 {
 	var assets = DKAssets_GetDataPath();
-	//DKLog(assets+"Documents"+"\n");
 	var docs = DKFile_DirectoryContents(assets+"Documents");
 	var htmldocs = DKFile_DirectoryContents("/home/keithnam/www/Documents");
 	docs = docs.concat(htmldocs);
 	//DKLog("FILE DOCS: "+docs+"\n");
+	
 	var files = docs.split(",");
 	for(i=0; i<files.length; i++){
 		if(files[i] == "."){ continue; }
 		if(files[i] == ".."){ continue; } 
-		var id = DKWidget_CreateElement("documents_div", "div", "fileID");
-		DKWidget_SetInnerHtml(id, files[i]);
+		//var div = DKWidget_CreateElement("documents_div", "div", "fileID");
+		var href = DKWidget_CreateElement("documents_div", "a", "fileID");
+		var br = DKWidget_CreateElement("documents_div", "br", "");
+		DKWidget_SetAttribute(href, "href", "test"); //<a href="url">link text</a>
+		DKWidget_SetInnerHtml(href, "link");
+		//DKWidget_SetOuterHtml(href, "<br>");
 	}
-	
-	/*
-	if(serverpath){
-		var htmlassets = serverpath;
-		//DKLog(htmlassets+"\n");
-		var htmldocs = DKFile_DirectoryContents("/home/keithnam/www/Documents");
-		DKLog("FILE DOCS: "+htmldocs+"\n");
-		var files = htmldocs.split(",");
-		for(i=0; i<files.length; i++){
-			if(files[i] == "."){ continue; }
-			if(files[i] == ".."){ continue; } 
-			var id = DKWidget_CreateElement("documents_div", "div", "fileID");
-			DKWidget_SetInnerHtml(id, files[i]);
-		}
-	}
-	*/
 }
 
 ////////////////////////////
