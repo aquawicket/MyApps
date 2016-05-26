50303ee1681732b0b44981c34b29ddbcc922b454
aquawicket <aquawicket@hotmail.com>
2016-05-25 19:58:19 -0700

    update

--- DKApps/UsGreenFunds/assets/Admin.js
+++ DKApps/UsGreenFunds/assets/Admin.js
@@ -3,7 +3,10 @@ DKCreate("DKWidget,Admin.html");
 ////////////////////
 function Admin_Init()
 {
-
+	var assets = DKAssets_GetDataPath();
+	DKLog(assets+"Documents"+"\n");
+	var docs = DKFile_DirectoryContents(assets+"Documents")
+	DKLog(docs+"\n");
 }
 
 ////////////////////////////
