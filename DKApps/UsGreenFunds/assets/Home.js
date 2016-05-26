f1fb34a131ca7bb00372a5eaf85cd4b6749aa16c
aquawicket <aquawicket@hotmail.com>
2016-05-25 19:28:39 -0700

    update

--- DKApps/UsGreenFunds/assets/Home.js
+++ DKApps/UsGreenFunds/assets/Home.js
@@ -1,1 +1,13 @@
 DKCreate("DKWidget,Home.html");
+
+////////////////////
+function Home_Init()
+{
+	DKRegisterEvent("login", "click", Home_OnEvent);
+}
+
+////////////////////////////
+function Home_OnEvent(event)
+{
+	DKLog("event");
+}
