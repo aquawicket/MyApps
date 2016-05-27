54e60c052202f4b7ceb2d4b88f16d496dd7228ca
aquawicket <aquawicket@hotmail.com>
2016-05-27 15:44:45 -0700

    update

--- DKApps/UsGreenFunds/assets/Documents.js
+++ DKApps/UsGreenFunds/assets/Documents.js
@@ -38,7 +38,7 @@ function Documents_Update()
 		if(files[i] == "."){ continue; }
 		if(files[i] == ".."){ continue; }
 		var div = DKWidget_CreateElement("documents_div", "div", "div");
-		DKWidget_SetProperty(div,"width","1px");
+		DKWidget_SetProperty(div,"width","80%");
 		DKWidget_SetProperty(div,"white-space","nowrap");
 	
 		var del = DKWidget_CreateElement(div, "img", "delete_"+files[i]);
