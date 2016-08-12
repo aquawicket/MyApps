52bba0ea5721156a3e81af565da2fa5761e921cf
aquawicket <aquawicket@hotmail.com>
2016-08-12 10:27:27 -0700

    update

---
+++ DKApps/BemisLaw/android32/Release/src/digitalknob/dkapp/WebviewActivity.java
@@ -0,0 +1,82 @@
+// AUTO-GENERATED FILE. "dkapp" is renamed.
+package digitalknob.dkapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class WebviewActivity extends Activity {
+
+    String homepage = "https://m.facebook.com/home.php";
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        mWebView=(WebView)findViewById(R.id.webview_webview);
+
+        mWebView.setInitialScale(1);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //mWebView.getSettings().setMinimumFontSize(40);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        //Only disabled the horizontal scrolling:
+        //mWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+
+        mWebView.setVerticalScrollBarEnabled(false);
+        mWebView.setHorizontalScrollBarEnabled(false);
+
+        mWebView.getSettings().setAppCacheEnabled(false);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+		//Location
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+		
+        mWebView.loadUrl(homepage);
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        String webUrl = mWebView.getUrl();
+        //Toast.makeText(this, webUrl, Toast.LENGTH_SHORT).show();
+        if (mWebView.canGoBack() && !webUrl.contains(homepage)){
+            mWebView.goBack();
+        }else {
+            mWebView.scrollTo(0,0);
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        }
+    }
+}
