7709f6ffc9f65aac5c5f9e22a6554480d57afcce
aquawicket <aquawicket@hotmail.com>
2016-09-03 00:41:33 -0700

    update

---
+++ DKApps/DKSDLBrowser/assets/User.js
@@ -0,0 +1,7 @@
+DKCreate("DKWindow");
+DKCreate("DKRocket");
+DKCreate("DKFrame/DKFrame.js");
+DKCreate("DKBrowser/DKBrowser.js");
+DKWidget_SetProperty("DKBrowser.html", "width", "100%");
+DKWidget_SetProperty("DKBrowser.html", "height", "100%");
+DKCreate("DKUpdate")
