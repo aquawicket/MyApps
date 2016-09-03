2ff94bf2b66ebde7e8c0899e30813c80e60adf4d
aquawicket <aquawicket@hotmail.com>
2016-09-02 20:08:29 -0700

    update

--- DKApps/DKReceiver/assets/DKReceiver/Receiver.js
+++ DKApps/DKReceiver/assets/DKReceiver/Receiver.js
@@ -4,25 +4,12 @@ DKCreate("DKReceiver/Receiver.html");
 //////////////////////
 function Receiver_Init()
 {
-	DKWidget_Hide("Go");
-	
-	//attempt to connect
-	if(Remote_server){
-		DKCreate("DKServer");
-		var IP = DK_GetLocalIP();
-		DKWidget_SetInnerHtml("IPAddress", IP);
-		DKWidget_Hide("Wifi");
-		DKWidget_Hide("address");
-	}
-	else{
-		DKCreate("DKClient");
-		var assets = DKAssets_LocalAssets();
-		var address = DKFile_GetSetting(assets+"remote.txt", "[SERVER]");
-		if(address){
-			DKWidget_SetValue("address", address);
-			//Receiver_Connect();  FIXME: crashes android 
-		}
-	}
+	DKWidget_Hide("Go");	
+	DKCreate("DKServer");
+	var IP = DK_GetLocalIP();
+	DKWidget_SetInnerHtml("IPAddress", IP);
+	DKWidget_Hide("Wifi");
+	DKWidget_Hide("address");
 	
 	DKAddEvent("GLOBAL", "server", Receiver_OnEvent);
 	DKAddEvent("GLOBAL", "client", Receiver_OnEvent);
@@ -42,25 +29,14 @@ function Receiver_OnEvent(event)
 	}
 	if(DK_Id(event, "VolumeUp_Button")){
 		DKLog("Button: VolumeUp \n", DKDEBUG);
-		if(Remote_server){
-			var volume = DK_GetVolume();
-			DK_ChangeVolume(volume+0.1);
-		}
-		else{
-			DKClient_Send("VolumeUp");
-		}
+		var volume = DK_GetVolume();
+		DK_ChangeVolume(volume+0.1);
 	}
 	if(DK_Id(event, "VolumeDown_Button")){
 		DKLog("Button: Volume Down\n", DKDEBUG);
-		if(Remote_server){
-			var volume = DK_GetVolume();
-			DK_ChangeVolume(volume-0.1);
-		}
-		else{
-			DKClient_Send("VolumeDown");
-		}	
+		var volume = DK_GetVolume();
+		DK_ChangeVolume(volume-0.1);
 	}
-
 	if(DK_Id(event, "Wifi")){
 		DKLog("Button: Wifi\n", DKDEBUG);
 		DKWidget_Toggle("address");
@@ -92,20 +68,20 @@ function Receiver_OnEvent(event)
 		DKLog("client: "+DKWidget_GetValue(event)+"\n", DKDEBUG);
 		if(DKWidget_GetValue(event) == "connected"){
 			DKWidget_Hide("address");
-			var datapath = DKAssets_GetDataPath();
-			DKWidget_SetAttribute("Wifi", "src", datapath+"DKReceiver/wifiGreen.png");
+			var assets = DKAssets_LocalAssets();
+			DKWidget_SetAttribute("Wifi", "src",  assets+"DKReceiver/wifiGreen.png");
 			//DK_CallFunc("DKOSGRocket::DirtyRefresh","");
 			var address = DKWidget_GetValue("address");
-			DKFile_SetSetting(datapath+"remote.txt", "[SERVER]", address); //provide full path in case file does not exist
+			DKFile_SetSetting( assets+"remote.txt", "[SERVER]", address); //provide full path in case file does not exist
 		}
 		if(DKWidget_GetValue(event) == "disconnected"){
 			DKWidget_Show("address");
-			DKWidget_SetAttribute("Wifi", "src", datapath+"DKReceiver/wifiRed.png");
+			DKWidget_SetAttribute("Wifi", "src",  assets+"DKReceiver/wifiRed.png");
 		}
 	}
 }
 
-/////////////////////////
+///////////////////////////
 function Receiver_Connect()
 {
 	DKClient_Connect( DKWidget_GetValue("address") );
