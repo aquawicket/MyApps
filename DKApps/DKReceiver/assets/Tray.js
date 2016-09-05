4ac1ef1134894317d3e73e833bfe8b62efdc5f96
aquawicket <aquawicket@hotmail.com>
2016-09-04 19:23:41 -0700

    update

---
+++ DKApps/DKReceiver/assets/Tray.js
@@ -0,0 +1,50 @@
+/////////////////////
+function Tray_Init()
+{
+	DKCreate("DKTray");
+	DKAddEvent("DKTray", "click", Tray_OnEvent);
+	DKAddEvent("DKTray", "Restore", Tray_OnEvent);
+	DKAddEvent("DKTray", "Minimize", Tray_OnEvent);
+	DKAddEvent("DKTray", "Exit", Tray_OnEvent);
+	Tray_Off();
+}
+
+////////////////////////////
+function Tray_OnEvent(event)
+{
+	//DKLog("DKTray_OnEvent("+event+") \n");
+	if(DK_Type(event, "click")){
+		Tray_ToggleIcon();
+	}
+}
+
+//////////////////////////
+function Tray_ToggleIcon()
+{
+	var icon = DKTray_GetIcon();
+	var file = DKFile_GetFilename(icon);
+	//DKLog(file+"\n");
+	
+	if(file == "touchON.ico"){
+		Tray_Off();
+	}
+	else{
+		Tray_On();
+	}
+}
+
+//////////////////
+function Tray_On()
+{
+	DKLog("ON \n");
+	var datapath = DKAssets_GetDataPath();
+	DKTray_SetIcon(datapath+"touchON.ico");
+}
+
+///////////////////
+function Tray_Off()
+{
+	DKLog("OFF \n");
+	var datapath = DKAssets_GetDataPath();
+	DKTray_SetIcon(datapath+"touchOFF.ico");
+}
