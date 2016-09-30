a0d942d63205495982ebcce8c7312478fd5b5fe2
aquawicket <aquawicket@hotmail.com>
2016-09-29 17:42:36 -0700

    update

--- DKApps/DKDatabase/assets/User.js
+++ DKApps/DKDatabase/assets/User.js
@@ -1,4 +1,23 @@
+var USE_CEF = false;
 DKCreate("DKWindow");
 DKCreate("DKRocket");
-DKCreate("DKScale/DKScale.js", function(){});
-DKCreate("DKDatabase/DKDatabase.js", function(){});
+if(DK_GetBrowser() == "DigitalKnob" && USE_CEF){
+	DKCreate("DKWidget");
+	var url = "file:///C:/digitalknob/USER/DKApps/DKDatabase/assets/index.html";
+	var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
+	DKWidget_SetAttribute(iframe, "src", url);
+	DKWidget_SetAttribute(iframe, "width", "100%");
+	DKWidget_SetAttribute(iframe, "height", "100%");
+	DKWidget_SetProperty(iframe, "position", "absolute");
+	DKWidget_SetProperty(iframe, "top", "0rem");
+	DKWidget_SetProperty(iframe, "left", "0rem");
+	DKWidget_SetProperty(iframe, "width", "100%");
+	DKWidget_SetProperty(iframe, "height", "100%");
+	var currentBrowser = DKCef_GetCurrentBrowser(iframe);
+	DKCef_SetUrl(iframe, url, currentBrowser);
+	DKCef_SetFocus(iframe);
+}
+else{
+	DKCreate("DKScale/DKScale.js", function(){});
+	DKCreate("DKDatabase/DKDatabase.js", function(){});
+}
