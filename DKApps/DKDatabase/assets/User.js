832fb5365635a757d80def11830f218613292b43
aquawicket <aquawicket@hotmail.com>
2016-10-03 14:46:56 -0700

    update

--- DKApps/DKDatabase/assets/User.js
+++ DKApps/DKDatabase/assets/User.js
@@ -7,20 +7,15 @@ if(DK_GetBrowser() == "DigitalKnob" && USE_CEF){
 	var url = "file:///C:/digitalknob/USER/DKApps/DKDatabase/assets/index.html";
 	//var url = "http://digitalknob.com/DKDatabase";
 	var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
-	DKWidget_SetAttribute(iframe, "src", url);
-	DKWidget_SetAttribute(iframe, "width", "100%");
-	DKWidget_SetAttribute(iframe, "height", "100%");
 	DKWidget_SetProperty(iframe, "position", "absolute");
 	DKWidget_SetProperty(iframe, "top", "0rem");
 	DKWidget_SetProperty(iframe, "left", "0rem");
 	DKWidget_SetProperty(iframe, "width", "100%");
 	DKWidget_SetProperty(iframe, "height", "100%");
-	//var currentBrowser = DKCef_GetCurrentBrowser(iframe);
-	//DKCef_SetUrl(iframe, url, currentBrowser);
+	DKCef_SetUrl(iframe, url, DKCef_GetCurrentBrowser(iframe));
 	DKCef_SetFocus(iframe);
 	
 	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
-		var width = DKWindow_GetWidth();
 		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
 		DKWidget_RemoveProperty(id, "top");
 		DKWidget_SetProperty(id, "bottom", "0rem");
@@ -29,11 +24,9 @@ if(DK_GetBrowser() == "DigitalKnob" && USE_CEF){
 else{
 	DKCreate("DKScale/DKScale.js", function(){});
 	DKCreate("DKDatabase/DKDatabase.js", function(){
-	
 		if(DK_GetBrowser() != "CEF"){
 			DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
-				var width = DKWindow_GetWidth();
-				var id = DKGoogleAd_CreateAd("DKDatabase.html", "100%", "100rem");
+				var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
 				DKWidget_RemoveProperty(id, "top");
 				DKWidget_SetProperty(id, "bottom", "0rem");
 			});
