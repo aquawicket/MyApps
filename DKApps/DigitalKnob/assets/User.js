92864c9edf3efea42f8e672811660cf47da7ffb9
aquawicket <aquawicket@hotmail.com>
2016-10-26 15:34:55 -0700

    update

--- DKApps/DigitalKnob/assets/User.js
+++ DKApps/DigitalKnob/assets/User.js
@@ -1,5 +1,6 @@
 DKCreate("DKWindow");
 DKCreate("DKRocket");
+DKCreate("DKWidget");
 DKCreate("DKDebug/DKDebug.js", function(){});
 DKCreate("DKScale/DKScale.js", function(){});
 DKCreate("Digitalknob/Digitalknob.js", function(){});
