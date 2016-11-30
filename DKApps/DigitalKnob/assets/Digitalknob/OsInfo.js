232d9ade9ef667781e9aba437845e56cedb907cf
aquawicket <aquawicket@hotmail.com>
2016-11-30 13:57:21 -0800

    update

--- DKApps/DigitalKnob/assets/Digitalknob/OsInfo.js
+++ DKApps/DigitalKnob/assets/Digitalknob/OsInfo.js
@@ -26,7 +26,7 @@ function OsInfo_OnEvent(event)
 function OsInfo_Resize()
 {
 	DKWidget_SetInnerHtml("OsInfo_OS", "OS: "+DK_GetOS());
-	DKWidget_SetInnerHtml("OsInfo_Browser", "Browser: "+DK_GetBrowser());
+	DKWidget_SetInnerHtml("OsInfo_Browser", "Browser: "+DK_GetBrowser()+" ,"+DK_GetJavascript());
 	DKWidget_SetInnerHtml("OsInfo_width", "width: "+DKWindow_GetWidth());
 	DKWidget_SetInnerHtml("OsInfo_height", "height: "+DKWindow_GetHeight());
 	DKWidget_SetInnerHtml("OsInfo_pixelratio", "pixel_ratio: "+DKWindow_GetPixelRatio());
