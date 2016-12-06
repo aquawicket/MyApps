84bb77915754bbe618e5d68d143b378a1e4075e0
aquawicket <aquawicket@hotmail.com>
2016-12-05 18:46:32 -0800

    update

--- DKApps/DKReceiver/assets/Tray.js
+++ DKApps/DKReceiver/assets/Tray.js
@@ -5,6 +5,8 @@ function Tray_Init()
 	DKAddEvent("DKTray", "1000", Tray_OnEvent);
 	DKAddEvent("DKTray", "1001", Tray_OnEvent);
 	DKAddEvent("DKTray", "1002", Tray_OnEvent);
+	DKAddEvent("DKTray", "click", Tray_OnEvent);
+	DKAddEvent("DKTray", "doubleclick", Tray_OnEvent);
 	
 	DKTray_AddItem("Exit", 1002);
 	DKTray_AddItem("Minimize", 1001);
@@ -17,7 +19,7 @@ function Tray_Init()
 function Tray_OnEvent(event)
 {
 	//DKLog("DKTray_OnEvent("+event+") \n");
-	if(DK_Type(event, "1000")){
+	if(DK_Type(event, "1000") || DK_Type(event, "doubleclick")){
 		DKCreate("DKWindowJS");
 		DKWindow_Show();
 	}
