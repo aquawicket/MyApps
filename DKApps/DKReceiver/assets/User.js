ebe149315a9d8e0c169adb7a0cdd30766e5a35b4
aquawicket <aquawicket@hotmail.com>
2016-12-05 19:06:51 -0800

    update

--- DKApps/DKReceiver/assets/User.js
+++ DKApps/DKReceiver/assets/User.js
@@ -2,4 +2,4 @@ DKCreate("DKWindow");
 DKCreate("DKRocket");
 DKCreate("DKScale/DKScale.js", function(){});
 DKCreate("DKReceiver/Receiver.js", function(){});
-DKCreate("Tray.js", function(){});
+DKCreate("DKTray/DKTray.js", function(){});
