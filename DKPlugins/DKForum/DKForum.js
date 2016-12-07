16ad0d6640ccd2b5a3892b6a914ca9ae2b084d12
aquawicket <aquawicket@hotmail.com>
2016-12-07 01:19:42 -0800

    update

---
+++ DKPlugins/DKForum/DKForum.js
@@ -0,0 +1,19 @@
+
+///////////////////////
+function DKForum_Init()
+{
+	//DKLog("DKForum_Init() \n");
+	DKCreate("DKForum/DKForum.html");
+}
+
+//////////////////////
+function DKForum_End()
+{
+	DKClose("DKForum/DKForum.html");
+}
+
+///////////////////////////////
+function DKForum_OnEvent(event)
+{
+	
+}
