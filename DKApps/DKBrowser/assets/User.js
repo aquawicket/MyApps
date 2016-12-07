f45d4509cf268934aa0300c916e5001a9ee042ee
aquawicket <aquawicket@hotmail.com>
2016-12-06 20:19:46 -0800

    update

--- DKApps/DKBrowser/assets/User.js
+++ DKApps/DKBrowser/assets/User.js
@@ -5,4 +5,5 @@ DKCreate("DKBrowser/DKBrowser.js");
 DKWidget_SetProperty("DKBrowser.html", "width", "100%");
 DKWidget_SetProperty("DKBrowser.html", "height", "100%");
 DKCreate("DKTray/DKTray.js");
+DKCreate("DKDebug/DKDebug.js");
 DKCreate("DKUpdate")
