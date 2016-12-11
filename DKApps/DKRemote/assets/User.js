8117f4c051d6ac376e023443c5de03e8f3e8fe96
aquawicket <aquawicket@hotmail.com>
2016-12-11 14:35:17 -0800

    update

--- DKApps/DKRemote/assets/User.js
+++ DKApps/DKRemote/assets/User.js
@@ -1,5 +1,6 @@
 DKCreate("DKWindow");
 DKCreate("DKRocket");
+DKCreate("DKWidget");
 DKCreate("DKScale/DKScale.js", function(){});
 DKCreate("DKRemote/Remote.js", function(){});
 DKCreate("DKDebug/DKDebug.js", function(){});
