89bfea8d7712f2f8ad529610af86e8a712b10e2e
aquawicket <aquawicket@hotmail.com>
2016-12-11 19:50:06 -0800

    update

--- DKApps/DKSDLWebview/assets/User.js
+++ DKApps/DKSDLWebview/assets/User.js
@@ -1,24 +1,5 @@
-if(DK_GetBrowser() == "Rocket"){
-	DKCreate("DKWindow");
-	DKCreate("DKRocket");
-	DKCreate("DKWidget");
-}
-
-DKCreate("DKDebug/DKDebug.js", function(){});
-
-var assets = DKAssets_LocalAssets();
-var url = "http://www.google.com";
-
-//TODO
-/*
-var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
-DKWidget_SetAttribute(iframe, "src", url);
-DKWidget_SetProperty(iframe, "position", "absolute");
-DKWidget_SetProperty(iframe, "top", "0rem");
-DKWidget_SetProperty(iframe, "left", "0rem");
-DKWidget_SetProperty(iframe, "width", "100%");
-DKWidget_SetProperty(iframe, "bottom", "0rem");
-var currentBrowser = DKCef_GetCurrentBrowser(iframe);
-DKCef_SetUrl(iframe, url, currentBrowser);
-DKCef_SetFocus(iframe);
-*/
+DKCreate("DKWindow");
+DKCreate("DKRocket");
+DKCreate("DKTray/DKTray.js");
+DKCreate("DKDebug/DKDebug.js");
+DKCreate("youtube.html");
