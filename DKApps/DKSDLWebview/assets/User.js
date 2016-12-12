07e6de29681aff9383a38bffe75f4084b15cb849
aquawicket <aquawicket@hotmail.com>
2016-12-11 20:44:24 -0800

    update

--- DKApps/DKSDLWebview/assets/User.js
+++ DKApps/DKSDLWebview/assets/User.js
@@ -1,5 +1,10 @@
 DKCreate("DKWindow");
 DKCreate("DKRocket");
-DKCreate("DKTray/DKTray.js");
+DKCreate("DKWidget");
+DKCreate("DKFrame/DKFrame.js");
 DKCreate("DKDebug/DKDebug.js");
-DKCreate("youtube.html");
+
+DKCreate("DKNotepad/DKNotepad.js");
+DKWidget_SetProperty("DKNotepad.html", "width", "100%");
+DKWidget_SetProperty("DKNotepad.html", "height", "100%");
+//DKCreate("youtube.html");
