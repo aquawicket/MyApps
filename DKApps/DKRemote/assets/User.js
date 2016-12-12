da705e4f418b24d0af1cb0f7be99161a0f87eb1e
aquawicket <aquawicket@hotmail.com>
2016-12-12 03:19:54 -0800

    update

--- DKApps/DKRemote/assets/User.js
+++ DKApps/DKRemote/assets/User.js
@@ -4,3 +4,16 @@ DKCreate("DKWidget");
 DKCreate("DKScale/DKScale.js", function(){});
 DKCreate("DKRemote/Remote.js", function(){});
 DKCreate("DKDebug/DKDebug.js", function(){});
+
+DKAddEvent("GLOBAL", "keydown", User_OnEvent);
+////////////////////////////
+function User_OnEvent(event)  //Duktape
+{
+	DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
+	if(DK_Type(event, "keydown")){
+		//DKLog("keydown ="+DKWidget_GetValue(event)+"\n", DKINFO);
+		if(DKWidget_GetValue(event) == "4"){ //Exit for ANDROID
+		    DK_Exit();
+		}
+	}
+}
--- DKApps/DKSDLWebview/assets/User.js
+++ DKApps/DKSDLWebview/assets/User.js
@@ -13,7 +13,6 @@ DKAddEvent("GLOBAL", "keydown", User_OnEvent);
 function User_OnEvent(event)  //Duktape
 {
 	DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
-
 	if(DK_Type(event, "keydown")){
 		//DKLog("keydown ="+DKWidget_GetValue(event)+"\n", DKINFO);
 		if(DKWidget_GetValue(event) == "4"){ //Exit for ANDROID
