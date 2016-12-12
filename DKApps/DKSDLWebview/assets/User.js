d766fe711647e3d5167924ab89a70218ef5d600b
aquawicket <aquawicket@hotmail.com>
2016-12-11 22:30:53 -0800

    update

--- DKApps/DKSDLWebview/assets/User.js
+++ DKApps/DKSDLWebview/assets/User.js
@@ -7,4 +7,6 @@ DKCreate("DKDebug/DKDebug.js");
 DKCreate("DKNotepad/DKNotepad.js");
 DKWidget_SetProperty("DKNotepad.html", "width", "100%");
 DKWidget_SetProperty("DKNotepad.html", "height", "100%");
+
+DKCreate("DKWebview");
 //DKCreate("youtube.html");
