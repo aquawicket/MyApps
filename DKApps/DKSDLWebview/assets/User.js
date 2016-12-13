1de5ff9dac0eaf51b63d698a9a5c7b27f7b519c0
aquawicket <aquawicket@hotmail.com>
2016-12-12 19:26:16 -0800

    update

--- DKApps/DKSDLWebview/assets/User.js
+++ DKApps/DKSDLWebview/assets/User.js
@@ -7,19 +7,18 @@ DKCreate("DKNotepad/DKNotepad.js");
 DKWidget_SetProperty("DKNotepad.html", "width", "100%");
 DKWidget_SetProperty("DKNotepad.html", "height", "100%");
 
-
+/////////////////////////
+//ANDROID back key = exit
 DKAddEvent("GLOBAL", "keydown", User_OnEvent);
-////////////////////////////
 function User_OnEvent(event)  //Duktape
 {
-	DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
 	if(DK_Type(event, "keydown")){
-		//DKLog("keydown ="+DKWidget_GetValue(event)+"\n", DKINFO);
 		if(DKWidget_GetValue(event) == "4"){ //Exit for ANDROID
 		    DK_Exit();
 		}
 	}
 }
+//////////////////////////
 
 DKCreate("DKWebview");
 //DKCreate("youtube.html");
