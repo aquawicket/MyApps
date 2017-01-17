635282ed093b7b8a73a41d10a064cfe58290c814
aquawicket <aquawicket@hotmail.com>
2017-01-17 01:44:31 -0800

    commit from git

--- DKApps/DKBrowser/assets/User.js
+++ DKApps/DKBrowser/assets/User.js
@@ -4,6 +4,8 @@ DKCreate("DKFrame/DKFrame.js");
 DKCreate("DKBrowser/DKBrowser.js");
 DKWidget_SetProperty("DKBrowser.html", "width", "100%");
 DKWidget_SetProperty("DKBrowser.html", "height", "100%");
-DKCreate("DKTray/DKTray.js");
+if(DK_GetOS() == "Win32" || DK_GetOS() == "Win64"){
+	DKCreate("DKTray/DKTray.js");
+}
 DKCreate("DKDebug/DKDebug.js");
-DKCreate("DKUpdate")
+//DKCreate("DKUpdate");
