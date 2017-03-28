3a76aa8ccd40d0c7711a339534278f8efbf64316
dkuser <dkuser@digitalknob.com>
2017-03-28 12:54:38 -0700

    commit from git

--- DKPlugins/DKBrowser/Settings.js
+++ DKPlugins/DKBrowser/Settings.js
@@ -2,6 +2,7 @@
 function Settings_Init()
 {
 	DKCreate("DKBrowser/Settings.html");
+	DKAddEvent("UpdateButton", "click", Settings_OnEvent);
 }
 
 ///////////////////////
@@ -13,5 +14,15 @@ function Settings_End()
 ///////////////////////////////
 function Settings_OnEvent(event)
 {
-	DKLog("Settings_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
+	//DKLog("Settings_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKINFO);
+	
+	if(DK_Id(event, "UpdateButton")){
+		Settings_Update();
+	}
+}
+
+//////////////////////////
+function Settings_Update()
+{
+	DKLog("Settings_Update()\n", DKINFO);
 }
