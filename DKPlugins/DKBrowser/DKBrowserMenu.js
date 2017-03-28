41c3174d7ad3e39adfa036dfd083967dfed2f668
dkuser <dkuser@digitalknob.com>
2017-03-28 16:39:45 -0700

    commit from git

--- DKPlugins/DKBrowser/DKBrowserMenu.js
+++ DKPlugins/DKBrowser/DKBrowserMenu.js
@@ -16,7 +16,7 @@ function DKBrowserMenu_Init()
 function DKBrowserMenu_End()
 {
 	DKRemoveEvent("GLOBAL", "mousedown", DKBrowserMenu_OnEvent);
-	DKClose("DKBrowserMenu.html");
+	DKClose("DKBrowser/DKBrowserMenu.html");
 }
 
 /////////////////////////////////////
@@ -45,7 +45,7 @@ function DKBrowserMenu_OnEvent(event)
 			return;
 		}
 	}
-	DKClose("DKBrowserMenu.js");
+	DKClose("DKBrowser/DKBrowserMenu.js");
 }
 
 ////////////////////////////
