c7192d91d2a292044e432f29fcb3dc115a0a7fbb
dkuser <dkuser@digitalknob.com>
2017-04-08 19:17:44 -0700

    commit from git

--- DKApps/DigitalKnob/assets/User.js
+++ DKApps/DigitalKnob/assets/User.js
@@ -30,8 +30,8 @@ function User_OnEvent(event)  //Duktape
 
 if(DK_GetBrowser() == "Rocket" && USE_CEF){ //Duktape
 	var assets = DKAssets_LocalAssets();
-	var url = "file:///"+assets+"/index.html";
-	//var url = "http://digitalknob.com/Digitalknob/index.html";
+	//var url = "file:///"+assets+"/index.html";
+	var url = "http://digitalknob.com/Digitalknob/index.html";
 	var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
 	DKWidget_SetAttribute(iframe, "src", url);
 	DKWidget_SetAttribute(iframe, "width", "100%");
