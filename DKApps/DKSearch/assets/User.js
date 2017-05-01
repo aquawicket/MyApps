195ca21c7822d38238dc222c6acf404dbc8156d6
dkuser <dkuser@digitalknob.com>
2017-04-30 21:36:42 -0700

    commit from git

--- DKApps/DKSearch/assets/User.js
+++ DKApps/DKSearch/assets/User.js
@@ -1,10 +1,14 @@
+var USE_SDL = 1;
 var USE_ROCKET = 1;
 var USE_CEF = 1;
 var USE_Webview = 1;
 var DKApp_url = "file:///"+DKAssets_LocalAssets()+"/index.html";
 //var DKApp_url = "http://digitalknob.com/DKSearch/index.html";
+//var DKApp_url = "http://google.com";
+//var DKApp_url = "chrome://gpu";
 
 
+//Validate settings
 if(DK_GetOS() == "Android" || DK_GetOS() == "iOS"){
 	USE_CEF = 0; //not available for mobile devices
 }
@@ -13,68 +17,97 @@ else{
 }
 
 
-if(DK_GetJavascript() == "Duktape"){
-	DKCreate("DKWindow");
-	if(USE_ROCKET){
-		DKCreate("DKRocket");
-		DKCreate("DKWidget");
-	}
-	DKCreate("DKTray/DKTray.js", function(){});
-}
-DKCreate("DKDebug/DKDebug.js", function(){});
-
-
 ////////////////////////////
-function User_OnEvent(event)  //Duktape events
+function User_OnEvent(event)  //Duktape
 {
-	//DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
+	DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DKWidget_GetValue(event)+")\n", DKDEBUG);
+	
+	if(DK_Type(event, "DKCef_OnQueueNewBrowser")){
+		var currentBrowser = DKCef_GetCurrentBrowser("DKCef_frame");
+		DKCef_SetUrl("DKCef_frame", DKWidget_GetValue(event), currentBrowser);
+	}
 	if(DK_Type(event, "keydown")){
 		if(DKWidget_GetValue(event) == "4"){ //Exit for ANDROID
 		    DK_Exit();
 		}
 	}
 	if(DK_Type(event, "resize")){
-		//DKLog("User_OnEvent(): resize\n", DKINFO);
-		//DKCef_SetSize("CefSDL", 100, 100);
 		var width = DKWindow_GetWidth();
 		var height = DKWindow_GetHeight();
 		DK_CallFunc("CefSDL::OnResize", "0,0,"+String(width)+","+String(height));
 	}
 }
 
-if(DK_GetJavascript() == "Duktape" && USE_ROCKET && USE_CEF){  //Create Cef frame in Rocket
-	var assets = DKAssets_LocalAssets();
-	var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
-	DKWidget_SetAttribute(iframe, "src", DKApp_url);
-	DKWidget_SetAttribute(iframe, "width", "100%");
-	DKWidget_SetAttribute(iframe, "height", "100%");
-	DKWidget_SetProperty(iframe, "position", "absolute");
-	DKWidget_SetProperty(iframe, "top", "0rem");
-	DKWidget_SetProperty(iframe, "left", "0rem");
-	DKWidget_SetProperty(iframe, "width", "100%");
-	DKWidget_SetProperty(iframe, "height", "100%");
-	var currentBrowser = DKCef_GetCurrentBrowser(iframe);
-	DKCef_SetUrl(iframe, DKApp_url, currentBrowser);
-	DKCef_SetFocus(iframe);
+////////////////////////////////
+if(DK_GetJavascript() == "Duktape"){
+	if(USE_SDL && USE_ROCKET && USE_CEF){
+		DKLog("Creating SDL -> Rocket -> Cef -> GUI \n", DKINFO);
+		DKCreate("DKWindow");
+		DKCreate("DKRocket");
+		DKCreate("DKWidget");
+		var assets = DKAssets_LocalAssets();
+		var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
+		DKWidget_SetAttribute(iframe, "src", DKApp_url);
+		DKWidget_SetProperty(iframe, "position", "absolute");
+		DKWidget_SetProperty(iframe, "top", "0rem");
+		DKWidget_SetProperty(iframe, "left", "0rem");
+		DKWidget_SetProperty(iframe, "width", "100%");
+		DKWidget_SetProperty(iframe, "bottom", "0rem");
+		var currentBrowser = DKCef_GetCurrentBrowser(iframe);
+		DKCef_SetUrl(iframe, DKApp_url, currentBrowser);
+		DKCef_SetFocus(iframe);
+		DKAddEvent("GLOBAL", "DKCef_OnQueueNewBrowser", User_OnEvent);
+	}
+	else if(USE_SDL && USE_ROCKET){
+		DKLog("Creating SDL -> ROCKET -> GUI \n", DKINFO);
+		DKCreate("DKWindow");
+		DKCreate("DKRocket");
+		DKCreate("DKWidget");
+		LoadPage();
+	}
+	else if(USE_SDL && USE_CEF){
+		DKLog("Creating SDL -> CEF -> GUI \n", DKINFO);
+		DKCreate("DKWindow");
+		var width = DKWindow_GetWidth();
+		var height = DKWindow_GetHeight();
+		DKCreate("DKCef,CefSDL,0,0,"+width+","+height+","+DKApp_url);
+		var currentBrowser = DKCef_GetCurrentBrowser("CefSDL");
+		DKCef_SetUrl("CefSDL", DKApp_url, currentBrowser);
+		DKCef_SetFocus("CefSDL");
+		DKAddEvent("GLOBAL", "resize", User_OnEvent);
+	}
+	else if(USE_CEF){
+		DKLog("Creating CEF -> GUI \n", DKINFO);
+		var width = 800;
+		var height = 600;
+		DKCreate("DKCef,Cef,0,0,"+width+","+height+","+DKApp_url);
+		DK_SetFramerate(5);
+	}
+	else if(USE_Webview){
+		DKLog("Creating WEBVIEW -> GUI \n", DKINFO);
+		DKAddEvent("GLOBAL", "keydown", User_OnEvent);
+	}
+	
+	//DKCreate("DKTray/DKTray.js", function(){});
+	//DKCreate("DKDebug/DKDebug.js", function(){});
 }
-else if(DK_GetJavascript() == "Duktape" && USE_CEF){ //Create Cef frame in SDL
-	var width = DKWindow_GetWidth();
-	var height = DKWindow_GetHeight();
-	DKCreate("DKCef,CefSDL,0,0,"+width+","+height+","+DKApp_url);
-	var currentBrowser = DKCef_GetCurrentBrowser("CefSDL");
-	DKCef_SetUrl("CefSDL", DKApp_url, currentBrowser);
-	DKCef_SetFocus("CefSDL");
-	DKAddEvent("GLOBAL", "resize", User_OnEvent);
+else{  //V8 or Webview
+	LoadPage();
 }
-else if(DK_GetJavascript() == "Duktape" && USE_Webview){ //Duktape
-	DKAddEvent("GLOBAL", "keydown", User_OnEvent);  //Exit for ANDROID
-}
-else{  //Duktape or V8 or Webview
+
+function LoadPage()
+{
+	//DKLog("Loading page... \n", DKINFO);
+	DKWidget_SetProperty("body","background-color","grey");
 	DKCreate("DKScale/DKScale.js", function(){});
 	DKCreate("DKSearch/DKSearch.js", function(){});	
+	DKCreate("DKDebug/DKDebug.js", function(){});
+	
 	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
 		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
 		DKWidget_RemoveProperty(id, "top");
 		DKWidget_SetProperty(id, "bottom", "0rem");
 	});
+	
+	//DKCreate("DKUpdate");
 }
