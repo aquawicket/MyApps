91a3d1fae6616f6af3972b2917dfbe15e16c15de
dkuser <dkuser@digitalknob.com>
2017-05-01 18:29:33 -0700

    commit from git

--- DKPlugins/DKVncServer/DKVncServer.cpp
+++ DKPlugins/DKVncServer/DKVncServer.cpp
@@ -1,11 +1,15 @@
 #include "stdafx.h"
 #include "DKVncServer.h"
 
+#include <rfb/rfb.h>
 
 ///////////////////
 void DKVncServer::Init()
 {
-
+    rfbScreenInfoPtr server=rfbGetScreen(&DKApp::argc, DKApp::argv, 400, 300, 8, 3, 4);
+    server->frameBuffer=(char*)malloc(400*300*4);
+    rfbInitServer(server);
+    rfbRunEventLoop(server,-1,FALSE);
 }
 
 //////////////////
