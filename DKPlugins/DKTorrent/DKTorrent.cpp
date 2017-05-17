aef521e808e9dfe6727b27d0d216104090a17717
dkuser <dkuser@digitalknob.com>
2017-05-16 20:50:33 -0700

    commit from git

--- DKPlugins/DKTorrent/DKTorrent.cpp
+++ DKPlugins/DKTorrent/DKTorrent.cpp
@@ -1,6 +1,13 @@
 #include "stdafx.h"
 #include "DKTorrent.h"
 
+#include <libtorrent/session.hpp>
+#include <libtorrent/add_torrent_params.hpp>
+#include <libtorrent/torrent_handle.hpp>
+#include <libtorrent/alert_types.hpp>
+#include <libtorrent/bencode.hpp>
+#include <libtorrent/torrent_status.hpp>
+
 ////////////////////////
 void DKTorrent::Init()
 {
