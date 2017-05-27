9d7ab4fd5394a72b155c5b31ad5e8e0bcec9be7a
dkuser <dkuser@digitalknob.com>
2017-05-27 06:34:52 -0700

    commit from git

--- DKApps/AllStores/assets/AllStores.js
+++ DKApps/AllStores/assets/AllStores.js
@@ -35,26 +35,26 @@ function AllStores_DoSearch(string)
 	DKWidget_SetInnerHtml("AllStores_items", "");
 	
 	if(string){
-		AllStores_LetGoToArry("https://us.letgo.com/en/q/"+string, function(){
+		//AllStores_LetGoToArry("https://us.letgo.com/en/q/"+string, function(){
 		AllStores_CloseFiveToArry("https://www.close5.com/s/"+string, function(){
-		AllStores_OfferUpToArry("https://offerup.com/search/?q="+string, function(){
-		AllStores_CraigslistToArry("https://orangecounty.craigslist.org/search/sss?query="+string, function(){		
+		//AllStores_OfferUpToArry("https://offerup.com/search/?q="+string, function(){
+		//AllStores_CraigslistToArry("https://orangecounty.craigslist.org/search/sss?query="+string, function(){		
 			AllStores_ShowItems();
+		//});
 		});
-		});
-		});
-		})
+		//});
+		//})
 	}
 	else{
-		AllStores_LetGoToArry("https://us.letgo.com/en", function(){
+		//AllStores_LetGoToArry("https://us.letgo.com/en", function(){
 		AllStores_CloseFiveToArry("https://www.close5.com", function(){
-		AllStores_OfferUpToArry("https://offerup.com/", function(){
-		AllStores_CraigslistToArry("https://orangecounty.craigslist.org/search/sss", function(){
+		//AllStores_OfferUpToArry("https://offerup.com/", function(){
+		//AllStores_CraigslistToArry("https://orangecounty.craigslist.org/search/sss", function(){
 			AllStores_ShowItems();
+		//});
 		});
-		});
-		});
-		});
+		//});
+		//});
 	}
 }
 
@@ -79,14 +79,14 @@ function AllStores_LetGoToArry(url, callback)
 					item_data[3] = items[i].getElementsByClassName("city")[0].innerHTML;  //location
 					item_data[4] = items[i].getElementsByClassName("img portrait")[0].firstChild.src; //image
 					item_data[5] = items[i].getElementsByClassName("img portrait")[0];  //url
-					item_data[6] = "$0"; //price
+					//item_data[6] = "$0"; //price
 				}
 				if(items[i].getElementsByClassName("img landscape")[0]){
 					item_data[2] = items[i].getElementsByClassName("img landscape")[0].title;  //title
 					item_data[3] = items[i].getElementsByClassName("city")[0].innerHTML;  //location
 					item_data[4] = items[i].getElementsByClassName("img landscape")[0].firstChild.src; //image
 					item_data[5] = items[i].getElementsByClassName("img landscape")[0];  //url
-					item_data[6] = "$0"; //price
+					//item_data[6] = "$0"; //price
 				}
 				item_arry.push(item_data);
 			}				
@@ -127,7 +127,7 @@ function AllStores_CraigslistToArry(url, callback)
 				}
 				item_data[5] = "https://orangecounty.craigslist.org"+items[i].getElementsByClassName("result-image gallery")[0].href;  //url
 				item_data[5] = item_data[5].replace("file:///C:","");
-				item_data[6] = "$0"; //price
+				//item_data[6] = "$0"; //price
 				item_arry.push(item_data);
 			}	
 		}
@@ -146,7 +146,7 @@ function AllStores_CloseFiveToArry(url, callback)
 			
 			var items = div.getElementsByClassName("four wide large screen four wide widescreen four wide computer four wide tablet eight wide mobile column");
 			for(var i=0; i<items.length; i++){
-				//DKLog(items[i].innerHTML+"\n", DKINFO);
+				DKLog(items[i].innerHTML+"\n", DKINFO);
 				
 				var item_data = new Array();
 				item_data[0] = "id";
@@ -157,7 +157,9 @@ function AllStores_CloseFiveToArry(url, callback)
 				item_data[4] = items[i].getElementsByTagName("img")[0].src; //img
 				item_data[5] = "https://www.close5.com"+items[i].getElementsByTagName("a")[0].href; //url
 				item_data[5] = item_data[5].replace("file:///C:","");
-				item_data[6] = "$0"; //price
+				if(items[i].getElementsByTagName("span")[spans.length-2]){
+					item_data[6] = "$"+items[i].getElementsByTagName("span")[spans.length-2].innerHTML;  //price
+				}
 				item_arry.push(item_data);
 			}	
 		}
@@ -203,7 +205,7 @@ function AllStores_ShowItems()
 		var itemdiv = document.createElement('div');
 		itemdiv.style.display = "inline-block";
 		itemdiv.style.width = "230rem";
-		itemdiv.style.height = "400rem";
+		itemdiv.style.height = "420rem";
 		itemdiv.style.textAlign = "center";
 		itemdiv.style.margin = "4rem";
 		//itemdiv.style.borderStyle = "solid";
@@ -266,12 +268,20 @@ function AllStores_ShowItems()
 		itemtitle.style.overflow = "hidden";
 		infodiv.appendChild(itemtitle);
 		
+		var itemprice = document.createElement('span');
+		if(item_arry[i][6]){
+			itemprice.innerHTML = item_arry[i][6];
+		}
+		itemprice.style.display = "block";
+		itemprice.style.fontSize = "18rem";
+		itemdiv.appendChild(itemprice);
+		
 		var itemloc = document.createElement('span');
 		if(item_arry[i][3]){
 			itemloc.innerHTML = item_arry[i][3];
 		}
 		itemloc.style.display = "block";
-		itemloc.style.fontSize = "16rem";
+		itemloc.style.fontSize = "15rem";
 		itemdiv.appendChild(itemloc);
 		
 		document.getElementById("AllStores_items").appendChild(itemdiv);
