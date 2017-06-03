36edb67ce7c236d673de3aec93491b61f3551d88
dkuser <dkuser@digitalknob.com>
2017-06-02 17:05:47 -0700

    commit from git

--- DKApps/AllStores/assets/AllStores_options.js
+++ DKApps/AllStores/assets/AllStores_options.js
@@ -2,6 +2,7 @@
 function AllStores_options_Init()
 {
 	DKCreate("AllStores_options.html", function(){});
+	DKAddEvent("AllStores_options_price", "click", AllStores_options_OnEvent);
 }
 
 ////////////////////////////////
@@ -13,6 +14,10 @@ function AllStores_options_End()
 /////////////////////////////////////////
 function AllStores_options_OnEvent(event)
 {
-	DKLog("AllStores_options_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKINFO);
+	//DKLog("AllStores_options_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKINFO);
+	
+	if(DK_Id(event, "AllStores_options_price")){
+		DKLog(DKWidget_GetValue("AllStores_options_price")+"\n", DKINFO);
+	}
 	
 }
