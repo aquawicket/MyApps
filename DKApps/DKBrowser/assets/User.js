8b8cf2ae8c9b5d659ecf857dc965a799ddb62045
dkuser <dkuser@digitalknob.com>
2017-09-13 00:50:58 -0700

    commit from git

--- DKApps/DKBrowser/assets/User.js
+++ DKApps/DKBrowser/assets/User.js
@@ -1,6 +1,6 @@
 DKCreate("DKWindow");
 DKCreate("DKRocket");
-DKCreate("DKFrame/DKFrame.js");
+DKCreate("DKGui/DKFrame.js");
 DKCreate("DKBrowser/DKBrowser.js");
 DKWidget_SetProperty("DKBrowser.html", "width", "100%");
 DKWidget_SetProperty("DKBrowser.html", "height", "100%");
