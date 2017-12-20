535e2d2ba5d0f15b4b2b168ce589f90c672dc0cf
dkuser <dkuser@digitalknob.com>
2017-12-19 21:15:36 -0800

    commit from git

--- DKApps/DKYoutube/assets/app.js
+++ DKApps/DKYoutube/assets/app.js
@@ -1,5 +1,21 @@
-DKCreate("DKWindow");
-DKCreate("DKRocket");
-DKCreate("DKYoutube/youtube.html");
-DKCreate("DKTray/DKTray.js");
-DKCreate("DKDebug/DKDebug.js");
+var USE_CEF = 1;     //Desktop
+var USE_WEBVIEW = 1; //Android, iOS?
+var USE_SDL = 0;     //Use with caution
+var USE_ROCKET = 0;  //Use with caution
+var DKApp_url = "http://www.youtube.com";
+
+DKCreate("DK/init.js", function(){});
+
+///////////////////////
+function app_LoadPage()
+{
+	DKLog("app_LoadPage()\n");
+	
+	DKCreate("DKWindow/DKWindow.js", function(){
+	DKCreate("DKTray/DKTray.js", function(){
+	DKCreate("DKDebug/DKDebug.js", function(){
+		
+	});
+	});
+	});
+}
