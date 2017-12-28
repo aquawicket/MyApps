e7504ea4f1e413c8b8a76d292a371704c295a341
dkuser <dkuser@digitalknob.com>
2017-12-27 19:46:10 -0800

    commit from git

--- DKApps/DKFacebook/assets/app.js
+++ DKApps/DKFacebook/assets/app.js
@@ -2,7 +2,7 @@ var USE_CEF = 1;     //Desktop
 var USE_WEBVIEW = 1; //Android, iOS?
 var USE_SDL = 0;     //Use with caution
 var USE_ROCKET = 0;  //Use with caution
-var DKApp_url = "http://www.facebook.com";
+var DKApp_url = "http://facebook.com/home.php";
 
 DKCreate("DK/init.js", function(){});
 DK_SetFramerate(120);
