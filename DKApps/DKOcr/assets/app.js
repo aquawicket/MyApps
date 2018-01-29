b10e7eb21db17ca69bfaeb2607edfe4df4b326b0
dkuser <dkuser@digitalknob.com>
2018-01-29 10:41:26 -0800

    commit from git

---
+++ DKApps/DKOcr/assets/app.js
@@ -0,0 +1,26 @@
+var USE_CEF = 0;     //Desktop
+var USE_WEBVIEW = 0; //Android, iOS?
+var USE_SDL = 1;     //Use with caution
+var USE_ROCKET = 1;  //Use with caution
+
+DKCreate("DK/init.js", function(){});
+
+//////////////////////////
+function app_LoadPlugins()
+{
+	DKCreate("DKOcr");
+}
+
+///////////////////////
+function app_LoadPage()
+{
+	DKLog("app_LoadPage()\n");
+	
+	DKCreate("DKWindow/DKWindow.js", function(){
+	DKCreate("DKTray/DKTray.js", function(){
+	DKCreate("DKDebug/DKDebug.js", function(){
+
+	});
+	});
+	});
+}
