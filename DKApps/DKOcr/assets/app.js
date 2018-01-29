adf22aed43653805735979bcb9cd7ea732a3c90d
dkuser <dkuser@digitalknob.com>
2018-01-29 12:17:30 -0800

    commit from git

--- DKApps/DKOcr/assets/app.js
+++ DKApps/DKOcr/assets/app.js
@@ -6,6 +6,20 @@ var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"index.html";
 
 DKCreate("DK/init.js", function(){});
 
+///////////////////////////
+function app_OnEvent(event)
+{
+	DKLog("app_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+	
+	if(DK_Type(event, "OpenFile")){
+		var file = DK_GetValue(event);
+		DKLog("OpenFile: "+file+" \n");
+		DKPaint_Open(file);
+		var text = DKOcr_ImageToText(file);
+		DKWidget_SetAttribute("DKNotepad_Text", "value", text);
+	}
+}
+
 //////////////////////////
 function app_LoadPlugins()
 {
@@ -16,6 +30,7 @@ function app_LoadPlugins()
 function app_LoadPage()
 {
 	DKLog("app_LoadPage()\n");
+	DKAddEvent("GLOBAL", "OpenFile", app_OnEvent);
 	
 	DKCreate("DKWindow/DKWindow.js", function(){
 	DKCreate("DKGui/DKFrame.js", function(){
@@ -24,7 +39,7 @@ function app_LoadPage()
 		DKCreate("DKOcr")
 		DKWidget_SetProperty("DKPaint/DKPaint.html", "height", "50%");
 		var assets = DKAssets_LocalAssets();
-		DKPaint_Open(assets+"DKOcr/test.png");
+		//DKPaint_Open(assets+"DKOcr/test.png");
 	DKCreate("DKNotepad/DKNotepad.js", function(){
 		DKWidget_SetProperty("DKNotepad/DKNotepad.html", "top", "50%");
 	});
