f59d2593d546ddc142f7ea077e7ee9e2259c6777
dkuser <dkuser@digitalknob.com>
2018-01-29 10:21:39 -0800

    commit from git

--- DKApps/DKInputTest/assets/app.js
+++ DKApps/DKInputTest/assets/app.js
@@ -9,6 +9,12 @@ var DKApp_url = "file:///"+DKAssets_LocalAssets()+"/index.html";
 
 DKCreate("DK/init.js", function(){});
 
+//////////////////////////
+function app_LoadPlugins()
+{
+
+}
+
 ///////////////////////
 function app_LoadPage()
 {
