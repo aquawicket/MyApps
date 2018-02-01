a46a592cdaea0cb9b91b2bf6c609753823ec0b9c
dkuser <dkuser@digitalknob.com>
2018-02-01 01:37:47 -0800

    commit from git

---
+++ DKApps/DKGuessWho/assets/app.js
@@ -0,0 +1,20 @@
+var USE_CEF     = 1; //Desktop
+var USE_WEBVIEW = 0; //TODO: Android, iOS
+var USE_SDL     = 0; //Use with caution
+var USE_ROCKET  = 0; //Use with caution
+var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"/index.html";
+
+
+DKCreate("DK/init.js", function(){});
+
+//////////////////////////
+function app_LoadPlugins()
+{
+	//TODO
+}
+
+///////////////////////
+function app_LoadPage()
+{
+	//TODO
+}
