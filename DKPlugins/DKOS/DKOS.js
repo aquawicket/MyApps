b59e7a80949dca0ea93dc7d704c8b0e7ad4ed8d8
dkuser <dkuser@digitalknob.com>
2018-02-27 16:38:42 -0800

    commit from git

--- DKPlugins/DKOS/DKOS.js
+++ DKPlugins/DKOS/DKOS.js
@@ -1,23 +1,25 @@
-DKCreate("DK/DKBrowser.css");
-var OS = DK_GetOS();
-DKLog("OS="+OS+"\n");
-if(OS != "iOS" && OS != "Android"){
-	var screenwidth = DK_GetScreenWidth();
-	var screenheight = DK_GetScreenHeight();
-	var newwidth = parseInt(screenwidth)-100;
-	var newheight = parseInt(screenheight)-150;
-	DKWindow_SetWidth(newwidth.toString()+"px");
-	DKWindow_SetHeight(newheight.toString()+"px");
-}
+////////////////////
+function DKOS_Init()
+{
+	DKCreate("DK/DKBrowser.css");
+	var OS = DK_GetOS();
+	DKLog("OS="+OS+"\n");
+	if(OS != "iOS" && OS != "Android"){
+		var screenwidth = DK_GetScreenWidth();
+		var screenheight = DK_GetScreenHeight();
+		var newwidth = parseInt(screenwidth)-100;
+		var newheight = parseInt(screenheight)-150;
+		DKWindow_SetWidth(newwidth.toString()+"px");
+		DKWindow_SetHeight(newheight.toString()+"px");
+	}
 
-DKCreate("DKGui/DKFrame.js", function(){
-	DKCreate("DKOS/Desktop.js", function(){
-		DKCreate("DKOS/Taskbar.js", function(){
-			if(OS == "iOS" || OS == "Android"){
-				//DKWidget_SetScale(2);   FIXME
-			}
+	DKCreate("DKGui/DKFrame.js", function(){
+		DKCreate("DKOS/Desktop.js", function(){
+			DKCreate("DKOS/Taskbar.js", function(){
+				if(OS == "iOS" || OS == "Android"){
+					//DKWidget_SetScale(2);   FIXME
+				}
+			});
 		});
 	});
-});
-
-
+}
