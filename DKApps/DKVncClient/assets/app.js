b41d80694675c22c4b6ed63a74264065db830b58
dkuser <dkuser@digitalknob.com>
2018-03-15 10:26:35 -0700

    commit from git

--- DKApps/DKVncClient/assets/app.js
+++ DKApps/DKVncClient/assets/app.js
@@ -1,8 +1,8 @@
 DKCreate("DKWindow");
 DKWindow_Create();
-DKCreate("DKRocket");
-DKCreate("DKWidget");
-DKCreate("Login.js", function(){});
+//DKCreate("DKRocket");
+//DKCreate("DKWidget");
+//DKCreate("Login.js", function(){});
 
 DKCreate("DKDebug/DKDebug.js", function(){
 	DKCreate("DKVncClient", function(){
