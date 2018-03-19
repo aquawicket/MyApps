b10441f3f36611440ec6dc64a0cb79ac4b3760ef
dkuser <dkuser@digitalknob.com>
2018-03-18 18:22:30 -0700

    commit from git

--- DKApps/DKCurl/assets/app.js
+++ DKApps/DKCurl/assets/app.js
@@ -1,3 +1,8 @@
 DKCreate("DKCurl");
+
+//TODO
+var args = DK_GetArgs(); //Get arguments
+DKLog("args = "+args+"\n", DKINFO);
+
 DKCurl_Download("http://digitalKnob.com/Download/DKBuilder.exe", "C:/digitalknob/Download");
 DK_Exit();
