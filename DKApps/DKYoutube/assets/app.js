8c17e5c0ba4571681b67ba0ed7936901caa4b129
dkuser <dkuser@digitalknob.com>
2018-03-24 19:28:51 -0700

    commit from git

--- DKApps/DKYoutube/assets/app.js
+++ DKApps/DKYoutube/assets/app.js
@@ -2,8 +2,8 @@ var USE_CEF     = 1; //Desktop
 var USE_WEBVIEW = 0; //TODO: Android, iOS
 var USE_SDL     = 0; //Use with caution
 var USE_ROCKET  = 0; //Use with caution
-//var DKApp_url   = "http://www.youtube.com";
-var DKApp_url   = "http://www.youtube.com/tv";
+var DKApp_url   = "http://www.youtube.com";
+//var DKApp_url   = "http://www.youtube.com/tv";
 
 DKCreate("DK/init.js", function(){});
 
