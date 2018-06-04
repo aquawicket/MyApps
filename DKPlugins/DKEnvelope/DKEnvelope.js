14124d7f1665453a11360bc5952c02d1c2c05065
dkuser <dkuser@digitalknob.com>
2018-06-04 12:40:39 -0700

    commit from git

--- DKPlugins/DKEnvelope/DKEnvelope.js
+++ DKPlugins/DKEnvelope/DKEnvelope.js
@@ -2,7 +2,7 @@
 function DKEnvelope_Init()
 {
 	DKCreate("DKEnvelope/DKEnvelope.html");
-	//DKAddEvent("DKEnvelope.html", "blah", DKEnvelope_OnEvent);
+	DKAddEvent("printEnvelope", "click", DKEnvelope_OnEvent);
 }
 
 /////////////////////////
@@ -14,11 +14,44 @@ function DKEnvelope_End()
 //////////////////////////////////
 function DKEnvelope_OnEvent(event)
 {	
-	DKLog("DKEnvelope_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+	//DKLog("DKEnvelope_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
 
-	/*
-	if(DK_Id(event, "Blah")){
-		//TODO
+	if(DK_Id(event, "printEnvelope")){
+		DKEnvelope_PrintEnvelope();
 	}
-	*/
+}
+
+///////////////////////////////////
+function DKEnvelope_PrintEnvelope()
+{
+	DKLog("DKEnvelope_PrintEnvelope()");
+	DKWidget_Hide("DKGui/Desktop.html");
+	DKWidget_Hide("DKGui/Taskbar.html");
+	
+	//Create the blank background
+	var ele = DKWidget_CreateElement("body", "div", "envelope_background");
+	DKWidget_SetProperty(ele, "position", "absolute");
+	DKWidget_SetProperty(ele, "width", "100%");
+	DKWidget_SetProperty(ele, "height", "100%");
+	DKWidget_SetProperty(ele, "background-color", "rgb(255,255,255)");
+	DKWidget_SetProperty(ele, "z-index", "100");
+	
+	//Build the envelope and position it
+	var ele2 = DKWidget_CreateElement(ele, "div", "envelope");
+	DKWidget_SetProperty(ele2, "position", "absolute");
+	DKWidget_SetProperty(ele2, "top", "100rem");
+	DKWidget_SetProperty(ele2, "left", "200rem");
+	DKWidget_SetProperty(ele2, "width", "300rem");
+	DKWidget_SetProperty(ele2, "height", "600rem");
+	DKWidget_SetProperty(ele2, "background-color", "rgb(200,200,200)");
+	DKWidget_SetProperty(ele2, "z-index", "101");
+	
+	DKWidget_SetInnerHtml(ele2, "test");
+	
+	DKCef_Print();
+	
+	//DKWidget_RemoveElement(ele);
+	//DKWidget_Show("DKGui/Desktop.html");
+	//DKWidget_Show("DKGui/Taskbar.html");
+	
 }
