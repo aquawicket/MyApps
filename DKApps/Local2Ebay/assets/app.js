2015edd9bc3d2863b9df24e1b17ce90a81b9a367
dkuser <dkuser@digitalknob.com>
2018-09-17 19:00:19 -0700

    commit from git

--- DKApps/Local2Ebay/assets/app.js
+++ DKApps/Local2Ebay/assets/app.js
@@ -2,10 +2,9 @@ var USE_CEF     = 1; //Desktop
 var USE_WEBVIEW = 0; //TODO: Android, iOS
 var USE_SDL     = 0; //Use with caution
 var USE_ROCKET  = 0; //Use with caution
-var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"/index.html";
-//var DKApp_url = "http://digitalknob.com/DKIDE/index.html";
-//var DKApp_url = "http://google.com";
-//var DKApp_url = "chrome://gpu";
+//var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"/index.html";
+//var DKApp_url = "http://digitalknob.com/Loacl2Ebay/index.html";
+var DKApp_url   = "https://inlandempire.craigslist.org/d/for-sale/search/sss";
 
 DKCreate("DK/init.js", function(){});
 
@@ -20,12 +19,5 @@ function app_LoadPage()
 {
 	DKWidget_SetProperty("body","background-color","grey");
 	DKCreate("DKScale/DKScale.js", function(){});
-	DKCreate("DKFtp/DKFtp.js", function(){});
 	DKCreate("DKDebug/DKDebug.js", function(){});
-	
-	//if(DK_GetBrowser() != "CEF"){ 
-	//	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
-	//		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
-	//	});
-	//}
 }
