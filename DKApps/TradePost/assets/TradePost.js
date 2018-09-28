aa4f221d5ce8be49bc4e1c8d09bdd5f15a30ae30
dkuser <dkuser@digitalknob.com>
2018-09-28 16:14:58 -0700

    commit from git

--- DKApps/TradePost/assets/TradePost.js
+++ DKApps/TradePost/assets/TradePost.js
@@ -1,1 +1,17 @@
-// TODO
+/////////////////////////
+function TradePost_Init()
+{
+	DKCreate("TradePost.html");
+}
+
+////////////////////////
+function TradePost_End()
+{
+	DKClose("TradePost.html");
+}
+
+/////////////////////////////////
+function TradePost_OnEvent(event)
+{	DKLog("TradePost_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
+
+}
