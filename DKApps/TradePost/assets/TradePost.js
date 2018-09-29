ef703b267860d7c7125c763428c25ef05ff9d320
dkuser <dkuser@digitalknob.com>
2018-09-28 18:51:12 -0700

    commit from git

--- DKApps/TradePost/assets/TradePost.js
+++ DKApps/TradePost/assets/TradePost.js
@@ -2,6 +2,12 @@
 function TradePost_Init()
 {
 	DKCreate("TradePost.html");
+	DKAddEvent("AddItem", "click", TradePost_OnEvent);
+	
+	//Create Items folder if it does not exist.
+	if(!DKFile_Exists(DKAssets_LocalAssets()+"Items")){
+		DKFile_MkDir(DKAssets_LocalAssets()+"Items");
+	}
 }
 
 ////////////////////////
@@ -14,4 +20,19 @@ function TradePost_End()
 function TradePost_OnEvent(event)
 {	DKLog("TradePost_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
 
+	if(DK_Id(event, "AddItem")){
+		TradePost_AddItem();
+	}
+}
+
+////////////////////////////
+function TradePost_AddItem()
+{
+	DKLog("TradePost_AddItem\n");
+	
+	var i = 0;
+	while(DKFile_Exists(DKAssets_LocalAssets()+"Items/"+i)){
+		i++;
+	}
+	DKFile_MkDir(DKAssets_LocalAssets()+"Items/"+i);
 }
