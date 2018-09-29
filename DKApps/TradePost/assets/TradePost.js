ab5318bba8a4f6df50ccd45e0069bbaa850d744b
dkuser <dkuser@digitalknob.com>
2018-09-28 19:42:18 -0700

    commit from git

--- DKApps/TradePost/assets/TradePost.js
+++ DKApps/TradePost/assets/TradePost.js
@@ -46,21 +46,34 @@ function TradePost_UpdateList()
 	DKLog("TradePost_UpdateList\n");
 	
 	DKWidget_SetInnerHtml("ItemList", ""); //clear
-	for(var i = 0; i < 1000; i++){
-		if(DKFile_Exists(DKAssets_LocalAssets()+"Items/"+i)){
+	for(var row = 0; row < 1000; row++){
+		if(DKFile_Exists(DKAssets_LocalAssets()+"Items/"+row)){
 			
-			//DKLog(DKAssets_LocalAssets()+"Items/"+i);
-			var id = DKWidget_CreateElement("ItemList", "div", "Item"+i);
-			DKWidget_SetProperty(id, "display", "inline-block");
-			DKWidget_SetProperty(id, "width", "100%");
-			DKWidget_SetProperty(id, "min-width", "450rem");
+			//DKLog(DKAssets_LocalAssets()+"Items/"+r);
+			var div = DKWidget_CreateElement("ItemList", "div", "Item"+row);
+			DKWidget_SetProperty(div, "display", "inline-block");
+			DKWidget_SetProperty(div, "width", "100%");
+			DKWidget_SetProperty(div, "min-width", "450rem");
+			DKWidget_SetProperty(div, "height", "20rem");
+			DKWidget_SetProperty(div, "background-color", "white");
+			//DKWidget_SetProperty(div, "border-width", "1rem");
+			//DKWidget_SetProperty(div, "border-color", "black");
+			//DKWidget_SetProperty(div, "border-style", "solid");
+			//DKWidget_SetValue(div, row);
+			
+			var id = DKWidget_CreateElement(div, "div", "ItemId"+row);
+			DKWidget_SetProperty(id, "overflow-x", "hidden");
+			DKWidget_SetProperty(id, "width", "30rem");
 			DKWidget_SetProperty(id, "height", "18rem");
+			DKWidget_SetProperty(id, "display", "inline-block");
 			DKWidget_SetProperty(id, "background-color", "white");
 			DKWidget_SetProperty(id, "border-width", "1rem");
 			DKWidget_SetProperty(id, "border-color", "black");
 			DKWidget_SetProperty(id, "border-style", "solid");
-			DKWidget_SetValue(id, i);
-			
+			DKWidget_SetAttribute(id, "row", row);
+			DKWidget_SetAttribute(id, "column", 1);
+			DKWidget_SetValue(id, row);
+				
 			/*
 			for(var r=0; r < 10; r++){
 				var command = DKWidget_CreateElement(id, "input", "ItemValue"+i+r);
