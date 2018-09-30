463fe6a10e397ddb88d6c42cc650754afd4c2991
dkuser <dkuser@digitalknob.com>
2018-09-30 04:14:29 -0700

    commit from git

--- DKApps/TradePost/assets/app.js
+++ DKApps/TradePost/assets/app.js
@@ -12,16 +12,16 @@ DKCreate("DKUpdate");
 
 DKCef_SetUrl("DKBrowser_cef", DKAssets_LocalAssets()+"TradePost/index.html", DKCef_GetCurrentBrowser("DKBrowser_cef"));
 
-DK_RunJavascript("DKLog(\"Test\");");
-DK_RunJavascript("DKCef_SetFocus(\"DKBrowser_cef\");");
-//DK_RunJavascript("DKBrowser_NewTab();");
+DK_RunDuktape("DKLog(\"Test\");");
+DK_RunDuktape("DKCef_SetFocus(\"DKBrowser_cef\");");
 
-DKBrowser_NewTab();
+
+//DK_RunJavascript("DKBrowser_NewTab();");
 //DK_RunJavascript("DKCef_SetUrl(\"DKBrowser_cef\", \"https://offerup.com/\", DKCef_GetCurrentBrowser(\"DKBrowser_cef\"));");
 
 
-
 /*
+DKBrowser_NewTab();
 DKCef_SetUrl("DKBrowser_cef", "https://inlandempire.craigslist.org/d/for-sale/search/sss", DKCef_GetCurrentBrowser("DKBrowser_cef"));
 DKBrowser_NewTab();
 DKCef_SetUrl("DKBrowser_cef", "https://us.letgo.com/en", DKCef_GetCurrentBrowser("DKBrowser_cef"));
