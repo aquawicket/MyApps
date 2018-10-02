9478078f8bfd086919813862927b17254ed421fd
dkuser <dkuser@digitalknob.com>
2018-10-02 02:49:46 -0700

    commit from git

--- DKApps/TradePost/assets/TradePost/TradePost.js
+++ DKApps/TradePost/assets/TradePost/TradePost.js
@@ -346,55 +346,35 @@ function PostToCraigslist(title, price, city, zip, description, make, model, con
 	
 	//first Craigslist post page
 	if(url.indexOf("https://post.craigslist.org") != -1 && url.indexOf("s=type") != -1){
-		var button = document.querySelector('input[value="fso"]');
-		if(button){ button.click(); }
-		var submit = document.querySelector('button[name="go"]');
-		if(submit){ submit.click(); }
+		document.querySelector('input[value="fso"]').click();
+		document.querySelector('button[name="go"]').click();
 		return;
 	}
 	
 	//second Craigslist post page
 	if(url.indexOf("https://post.craigslist.org") != -1 && url.indexOf("s=cat") != -1){
-		var button = document.querySelector('input[value="96"]');
-		if(button){ button.click(); }
-		var submit = document.querySelector('button[name="go"]');
-		if(submit){ submit.click(); }
+		document.querySelector('input[value="96"]').click();
+		document.querySelector('button[name="go"]').click();
 		return;
 	}
 	
 	//third Craigslist post page
 	if(url.indexOf("https://post.craigslist.org") != -1 && url.indexOf("s=edit") != -1){
-		console.log(title);
-		var _title = document.querySelector('input[id="PostingTitle"]');
-		if(_title){ _title.value = title; }
-		var _price = document.querySelector('input[name="price"]');
-		if(_price){ _price.value = Number(price); }
-		var _city = document.querySelector('input[id="GeographicArea"]');
-		if(_city){ _city.value = city; }
-		var _zip = document.querySelector('input[id="postal_code"]');
-		if(_zip){ _zip.value = zip; }
-		var _description = document.querySelector('textarea[id="PostingBody"]');
-		if(_description){ _description.value = description; }
-		var _make = document.querySelector('input[name="sale_manufacturer"]');
-		if(_make){ _make.value = make; }
-		var _model = document.querySelector('input[name="sale_model"]');
-		if(_model){ _model.value = model; }
-		//var _condition = document.querySelector('select[name="condition"]');
-		//if(_condition){ _condition.value = "10"; }	
-		var _email = document.querySelector('input[name="FromEMail"]');
-		if(_email){ _email.value = email; }
-		var _email2 = document.querySelector('input[name="ConfirmEMail"]');
-		if(_email2){ _email2.value = email; }
-		var contact_text = document.querySelector('input[name="contact_text_ok"]');
-		if(contact_text){ contact_text.click(); }
-		var _phone = document.querySelector('input[name="contact_phone"]');
-		if(_phone){ _phone.value = phone; }
-		var _name = document.querySelector('input[name="contact_name"]');
-		if(_name){ _name.value = name; }
-		var _street = document.querySelector('input[name="xstreet0"]');
-		if(_street){ _street.value = street; }
-		var _city = document.querySelector('input[name="city"]');
-		if(_city){ _city.value = city; }
+		document.querySelector('input[id="PostingTitle"]').value = title;
+		document.querySelector('input[name="price"]').value = Number(price);
+		document.querySelector('input[id="GeographicArea"]').value = city;
+		document.querySelector('input[id="postal_code"]').value = zip;
+		document.querySelector('textarea[id="PostingBody"]').value = description;
+		document.querySelector('input[name="sale_manufacturer"]').value = make;
+		document.querySelector('input[name="sale_model"]').value = model;
+		//document.querySelector('select[name="condition"]').value = condition;
+		document.querySelector('input[name="FromEMail"]').value = email;
+		document.querySelector('input[name="ConfirmEMail"]').value = email;
+		document.querySelector('input[name="contact_text_ok"]').click();
+		document.querySelector('input[name="contact_phone"]').value = phone;
+		document.querySelector('input[name="contact_name"]').value = name;
+		document.querySelector('input[name="xstreet0"]').value = street;
+		document.querySelector('input[name="city"]').value = city;
 		return;
 	}
 	
