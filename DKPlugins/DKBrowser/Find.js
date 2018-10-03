861c15eaf70a00c9ee8330e858741c679a853df1
dkuser <dkuser@digitalknob.com>
2018-10-02 22:54:05 -0700

    commit from git

--- DKPlugins/DKBrowser/Find.js
+++ DKPlugins/DKBrowser/Find.js
@@ -20,7 +20,7 @@ function Find_OnEvent(event)
 	//DKLog("Find_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
 	
 	if(DK_Id(event, "FindNext")){
-		DKCef_Find("DKBrowser_cef", DKWidget_GetValue("FindInput"));
+		DKCef_Find("DKBrowser_cef", 0, DKWidget_GetValue("FindInput"));
 	}
 	if(DK_Id(event, "FindCancel")){
 		DKFrame_Close("DKBrowser/Find.html"); //FIXME: not working
