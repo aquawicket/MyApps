c835109d2abe8ae79cfe9de226f6ec101ab48808
dkuser <dkuser@digitalknob.com>
2018-10-03 06:51:16 -0700

    commit from git

--- DKApps/TradePost/assets/app.js
+++ DKApps/TradePost/assets/app.js
@@ -11,26 +11,8 @@ DKWidget_SetProperty("body", "background-color", "rgb:(120,120,120)");
 
 DKCreate("DKSDLText"); //This currently fixed cef redraw issues.
 
+DKCef_SetUrl("DKBrowser_cef", DKCef_GetCurrentBrowser("DKBrowser_cef"), DKAssets_LocalAssets()+"TradePost_index.html");
+
 //DK_SetFramerate(120);
 //DKCreate("DKUpdate");
 
-DKCef_SetUrl("DKBrowser_cef", DKCef_GetCurrentBrowser("DKBrowser_cef"), DKAssets_LocalAssets()+"TradePost_index.html");
-
-DK_RunDuktape("DKLog('Test');");
-DK_RunDuktape("DKCef_SetFocus('DKBrowser_cef');");
-
-
-//DK_RunDuktape("DKBrowser_NewTab();");
-//DK_RunDuktape("DKCef_SetUrl('DKBrowser_cef', DKCef_GetCurrentBrowser('DKBrowser_cef'), 'https://inlandempire.craigslist.org/d/for-sale/search/sss');");
-//DK_RunDuktape("DKBrowser_SelectTab(0);");
-
-
-/*
-DKBrowser_NewTab();
-DKCef_SetUrl("DKBrowser_cef", DKCef_GetCurrentBrowser("DKBrowser_cef"), "https://inlandempire.craigslist.org/d/for-sale/search/sss");
-DKBrowser_NewTab();
-DKCef_SetUrl("DKBrowser_cef", DKCef_GetCurrentBrowser("DKBrowser_cef"), "https://us.letgo.com/en");
-DKBrowser_NewTab();
-DKCef_SetUrl("DKBrowser_cef", DKCef_GetCurrentBrowser("DKBrowser_cef"), "https://offerup.com/");
-DKBrowser_SelectTab(0);
-*/
