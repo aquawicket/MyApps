653ba46aab11f392f28d3345abbf3a13c4e360e3
dkuser <dkuser@digitalknob.com>
2018-10-03 23:45:59 -0700

    commit from git

--- DKPlugins/DKBrowser/Settings.js
+++ DKPlugins/DKBrowser/Settings.js
@@ -1,13 +1,17 @@
 ////////////////////////
 function Settings_Init()
 {
+	//DKLog("Settings_Init()\n");
 	DKCreate("DKBrowser/Settings.html");
 	DKAddEvent("UpdateButton", "click", Settings_OnEvent);
+	DKAddEvent("VersionButton", "click", Settings_OnEvent);
+	DKAddEvent("GpuButton", "click", Settings_OnEvent);
 }
 
 ///////////////////////
 function Settings_End()
 {
+	//DKLog("Settings_End()\n");
 	DKClose("DKBrowser/Settings.html");
 }
 
@@ -15,17 +19,38 @@ function Settings_End()
 function Settings_OnEvent(event)
 {
 	//DKLog("Settings_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
-	
 	if(DK_Id(event, "UpdateButton")){
 		Settings_Update();
+	}
+	if(DK_Id(event, "VersionButton")){
+		Settings_Version();
+	}
+	if(DK_Id(event, "GpuButton")){
+		Settings_Gpu();
 	}
 }
 
 //////////////////////////
 function Settings_Update()
 {
-	DKLog("Settings_Update()\n");
+	//DKLog("Settings_Update()\n");
 	DKCreate("DKUpdate");
 	DKUpdate_CheckForUpdate();
 	DKUpdate_DoUpdate();
+}
+
+///////////////////////////
+function Settings_Version()
+{
+	//DKLog("Settings_Version()\n");
+	DKBrowser_NewTab(0);
+	DKCef_SetUrl("DKBrowser_cef", DKCef_GetCurrentBrowser("DKBrowser_cef"), "chrome://version");
+}
+
+///////////////////////
+function Settings_Gpu()
+{
+	//DKLog("Settings_Gpu()\n");
+	DKBrowser_NewTab(0);
+	DKCef_SetUrl("DKBrowser_cef", DKCef_GetCurrentBrowser("DKBrowser_cef"), "chrome://gpu");
 }
