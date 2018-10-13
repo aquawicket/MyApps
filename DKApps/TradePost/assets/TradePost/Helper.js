fcd6d83fd79afbaf408bfbecfe795b67b1257502
dkuser <dkuser@digitalknob.com>
2018-10-12 20:31:54 -0700

    commit from git

--- DKApps/TradePost/assets/TradePost/Helper.js
+++ DKApps/TradePost/assets/TradePost/Helper.js
@@ -109,3 +109,20 @@ function TestFunction(num, callback)
 	DKLog("####### TimesTwo("+num+") #######\n");
 	callback(result);
 }
+
+//////////////////////////////////
+function Pinger_ping(ip, callback)
+{
+	if(!this.inUse){
+		this.inUse = true;
+		this.callback = callback
+		this.ip = ip;
+		var _that = this;
+		this.img = new Image();
+		this.img.onload = function() {_that.good();};
+		this.img.onerror = function() {_that.good();};
+		this.start = new Date().getTime();
+		this.img.src = "http://" + ip;
+		this.timer = setTimeout(function() { _that.bad();}, 1500);
+	}
+}
