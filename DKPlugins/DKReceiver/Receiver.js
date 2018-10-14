e83a37b63d7d3a6c73960c555d3067df58f85122
dkuser <dkuser@digitalknob.com>
2018-10-14 04:43:17 -0700

    commit from git

--- DKPlugins/DKReceiver/Receiver.js
+++ DKPlugins/DKReceiver/Receiver.js
@@ -1,17 +1,26 @@
-
-//////////////////////
+////////////////////////
 function Receiver_Init()
 {
 	DKCreate("DKReceiver/Receiver.html");	
-	DKCreate("DKServer");
-	DKCreate("DKServerWebsockets"); //Test
+	//DKCreate("DKServer");
+	DKCreate("DKWebsockets");
+	DKWebSockets_CreateServer("127.0.0.1", 80);
 	var IP = DK_GetLocalIP();
 	DKWidget_SetInnerHtml("IPAddress", IP);
 	
-	DKAddEvent("GLOBAL", "server", Receiver_OnEvent);
+	//DKAddEvent("GLOBAL", "server", Receiver_OnEvent);
+	DKAddEvent("GLOBAL", "DKWebSockets_OnMessage", Receiver_OnEvent);
 	DKAddEvent("VolumeUp_Button", "click", Receiver_OnEvent);
 	DKAddEvent("VolumeDown_Button", "click", Receiver_OnEvent);
 
+}
+
+///////////////////////
+function Receiver_End()
+{
+	DKWebSockets_CloseServer();
+	DKRemoveEvents(Receiver_OnEvent);
+	DKClose("DKReceiver/Receiver.html");
 }
 
 ////////////////////////////////
@@ -40,7 +49,38 @@ function Receiver_OnEvent(event)
 		}
 	}
 	
+	/*
 	if(DK_Type(event, "server")){
+		DKLog("server: "+DK_GetValue(event)+"\n");
+		if(DK_GetValue(event) == "Power"){
+			DKLog("Client: Power\n");
+		}
+		if(DK_GetValue(event) == "VolumeUp"){
+			DKLog("Client: VolumeUp\n");
+			var volume = DK_GetVolume();
+			if(DK_GetOS() != "Linux"){
+				DK_SetVolume(volume+0.1);
+				DKTray_ShowBalloon("Volume Up");
+			}
+			else{
+				DK_SetVolume(volume+5000);
+			}
+		}
+		if(DK_GetValue(event) == "VolumeDown"){
+			DKLog("Client: VolumeDown\n");
+			var volume = DK_GetVolume();
+			if(DK_GetOS() != "Linux"){
+				DK_SetVolume(volume-0.1);
+				DKTray_ShowBalloon("Volume Down");
+			}
+			else{
+				DK_SetVolume(volume-5000);
+			}
+		}
+	}
+	*/
+	
+	if(DK_Type(event, "DKWebSockets_OnMessage")){
 		DKLog("server: "+DK_GetValue(event)+"\n");
 		if(DK_GetValue(event) == "Power"){
 			DKLog("Client: Power\n");
