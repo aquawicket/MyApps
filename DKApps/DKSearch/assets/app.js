af48ab34d5ef44330618971312f4bcfaab9c610f
dkuser <dkuser@digitalknob.com>
2018-10-22 03:04:29 -0700

    commit from git

--- DKApps/DKSearch/assets/app.js
+++ DKApps/DKSearch/assets/app.js
@@ -1,30 +1,37 @@
 var USE_CEF     = 1; //Desktop
 var USE_WEBVIEW = 0; //TODO: Android, iOS
 var USE_SDL     = 0; //Use with caution
-var USE_ROCKET  = 0; //Use with caution
+var USE_ROCKET  = 1; //Use with caution
 var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"/index.html";
 //var DKApp_url = "http://digitalknob.com/DKSearch
 
 DKCreate("DK/init.js", function(){});
 
+//////////////////////////
+function app_LoadPlugins()
+{
+	DKCreate("DKDebug/DKDebug.js", function(){});
+
+	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
+		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
+		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
+	});
+}
+
 ///////////////////////
 function app_LoadPage()
 {
-	DKLog("app_LoadPage()\n");
-	
 	DKWidget_SetProperty("body","background-color","grey");
 	DKCreate("DKWindow/DKWindow.js", function(){
 		DKCreate("DKScale/DKScale.js", function(){
 			DKCreate("DKDebug/DKDebug.js", function(){
-				DKCreate("DKSearch/DKSearch.js", function(){});
-				
-				DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
-					var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
-					DKWidget_RemoveProperty(id, "top");
-					DKWidget_SetProperty(id, "bottom", "0rem");
-				});
-
+				DKCreate("DKSearch/DKSearch.js", function(){});	
 			});
 		});
+	});
+
+	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
+		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
+		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
 	});
 }
