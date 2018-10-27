5dc4cf4adbde5c508ef8539892ebc5d936947b5c
dkuser <dkuser@digitalknob.com>
2018-10-26 20:44:53 -0700

    commit from git

--- DKApps/DKRemote/assets/app.js
+++ DKApps/DKRemote/assets/app.js
@@ -1,5 +1,6 @@
 DKCreate("DKWindow");
 DKCreate("DKRocket");
+DKRocket_LoadGui("index.html");
 DKCreate("DKWidget");
 DKCreate("DKScale/DKScale.js", function(){});
 DKCreate("DKRemote/Remote.js", function(){});
