1bfb34c7ed8d86ea002770bc877fc2b63f4badbc
dkuser <dkuser@digitalknob.com>
2018-10-28 03:00:57 -0700

    commit from git

--- DKApps/DKBrowser/assets/app.js
+++ DKApps/DKBrowser/assets/app.js
@@ -1,14 +1,5 @@
+// executable app startup script
+
 DKCreate("DKWindow");
 DKCreate("DKRocket");
 DKRocket_LoadGui("index.html");
-DKCreate("DK/DK.css");	
-DKCreate("DKGui/DKFrame.js");
-DKCreate("DKBrowser/DKBrowser.js");
-DKWidget_SetProperty("DKBrowser.html", "width", "100%");
-DKWidget_SetProperty("DKBrowser.html", "height", "100%");
-DKCreate("DKTray/DKTray.js");
-DKCreate("DKDebug/DKDebug.js");
-DKCreate("DKSDLText");
-DK_SetFramerate(120);
-DKCreate("DKUpdate");
-DKWidget_SetProperty("body", "background-color", "rgb:(245,245,245)");
