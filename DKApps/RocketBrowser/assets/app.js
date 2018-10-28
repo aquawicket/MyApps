e39dafed04d0c5e2a7fcb96205b52e4715c044a9
dkuser <dkuser@digitalknob.com>
2018-10-28 03:18:54 -0700

    commit from git

--- DKApps/RocketBrowser/assets/app.js
+++ DKApps/RocketBrowser/assets/app.js
@@ -5,8 +5,8 @@ DKCreate("DKDom/DKDom.js");
 
 //attmpt to load code from an online resource
 DKCreate("DKCurl");
-var str = DKCurl_HttpToString("http://digitalknob.com/test.html");
+var str = DKCurl_HttpToString("http://digitalknob.com/DKInputTest");
 var assets = DKAssets_LocalAssets();
 DKFile_StringToFile(str, assets+"test.html");
 
-DKRocket_LoadGui(assets+"test.html");
+DKRocket_LoadUrl(assets+"test.html");
