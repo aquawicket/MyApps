801bb0aaf888ed7769769149b073e2ed07bb9f02
dkuser <dkuser@digitalknob.com>
2018-10-28 05:12:10 -0700

    commit from git

--- DKApps/DKInputTest/assets/app.js
+++ DKApps/DKInputTest/assets/app.js
@@ -11,30 +11,9 @@ DKCreate("DK/init.js", function(){});
 function app_LoadPlugins()
 {
 	DKCreate("DKDebug/DKDebug.js", function(){});
-
+	DKCreate("DKWidget");
 	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
 		var id = DKGoogleAd_CreateAd("body", "100%", "90rem");
 		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
-	});
-}
-
-///////////////////////
-function app_LoadPage()
-{
-	DKWidget_SetProperty("body","background-color","grey");
-	DKCreate("DKWindow/DKWindow.js", function(){
-	DKCreate("DKScale/DKScale.js", function(){
-	DKCreate("DKDebug/DKDebug.js", function(){
-	DKCreate("appPage.html", function(){
-	DKCreate("DKInputTest/DKInput.js", function(){
-		DKWidget_AppendChild("dkapp_container", "DKInputTest/DKInput.html");
-	DKCreate("DKGoogleAd/DKGoogleAd.js", function(){
-		var id = DKGoogleAd_CreateAd("body", "100%", "90rem");
-		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
-	});
-	});
-	});
-	});
-	});
 	});
 }
