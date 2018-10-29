0637d5c57bc0a0c246c7ab2472c481727f104881
dkuser <dkuser@digitalknob.com>
2018-10-29 03:20:34 -0700

    commit from git

--- DKApps/RocketBrowser/assets/test.js
+++ DKApps/RocketBrowser/assets/test.js
@@ -1,11 +1,22 @@
-DKWARN("Loaded test.js\n");
+console.log("Loaded test.js\n");
 
-document.body.style.width = "100%";
+document.getElementsByTagName("html")[0].style.height = "100%";
+document.body.style.margin = "0px";
+document.body.style.top = "0px";
+document.body.style.overflow = "hidden";
+//document.body.style.bottom = "0px";
+//document.body.style.left = "0px";
+//document.body.style.right = "0px";
+//document.body.style.width = "100%";
 document.body.style.height = "100%";
 document.body.style.backgroundColor = "grey";
+document.body.style["border-width"] = "1px";
+document.body.style["border-style"] = "solid";
+document.body.style["border-color"] = "black";
 
 /*
 var button = document.createElement("button");
 button.innerHTML = "button";
+button.style.position = "absolute";
 document.body.appendChild(button);
 */
