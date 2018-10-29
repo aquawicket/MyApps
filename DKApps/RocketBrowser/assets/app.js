8f7992dc732e7196998b74fb58f4b33dd497f7f7
dkuser <dkuser@digitalknob.com>
2018-10-29 02:01:09 -0700

    commit from git

--- DKApps/RocketBrowser/assets/app.js
+++ DKApps/RocketBrowser/assets/app.js
@@ -3,3 +3,14 @@ DKCreate("DKRocket");
 
 //attmpt to load code from an online resource
 DKRocket_LoadUrl("test.html");
+
+DKCreate("DKWidget");
+var iframe = DKWidget_CreateElement("body", "iframe", "DKCef_frame");
+DKWidget_SetProperty(iframe, "position", "absolute");
+DKWidget_SetProperty(iframe, "top", "0rem");
+//DKWidget_SetProperty(iframe, "left", "0rem");
+DKWidget_SetProperty(iframe, "right", "0rem");
+DKWidget_SetProperty(iframe, "width", "50%");
+DKWidget_SetProperty(iframe, "height", "100%");
+DKWidget_SetAttribute(iframe, "src", "test.html");
+DKCef_SetFocus(DKCef_GetCurrentBrowser());
