417d3af26702fecbb49c5e81001be206a2dcad92
dkuser <dkuser@digitalknob.com>
2018-11-06 19:26:20 -0800

    commit from git

--- DKApps/RocketBrowser/assets/app.js
+++ DKApps/RocketBrowser/assets/app.js
@@ -1,10 +1,10 @@
 DKCreate("DKDebug/DKDebug.js");
 
 DKCreate("DKAssets");
-var url = DKAssets_LocalAssets()+"test.html";
+//var url = DKAssets_LocalAssets()+"test.html";
 //var url = "http://digitalknob.com/test.html";
 //var url = "http://this-page-intentionally-left-blank.org";
-//var url = "http://web.ics.purdue.edu/~gchopra/class/public/pages/webdesign/05_simple.html";
+var url = "http://web.ics.purdue.edu/~gchopra/class/public/pages/webdesign/05_simple.html";
 //var url = "https://www.google.com/";
 //var url = "https://curl.haxx.se/libcurl/c/https.html"
 
