e46b0fa7c1b50761ec9ce4d8bf5fef04186c849a
dkuser <dkuser@digitalknob.com>
2018-11-09 00:59:37 -0800

    commit from git

--- DKApps/RocketBrowser/assets/app.js
+++ DKApps/RocketBrowser/assets/app.js
@@ -1,16 +1,30 @@
 DKDuktape_Create("DKDebug/DKDebug.js");
 
 DKDuktape_Create("DKAssets");
-var url = DKAssets_LocalAssets()+"test.html";
+//var url = DKAssets_LocalAssets()+"test.html";
 //var url = "http://digitalknob.com/test.html";
 //var url = "http://this-page-intentionally-left-blank.org";
-//var url = "http://web.ics.purdue.edu/~gchopra/class/public/pages/webdesign/05_simple.html";
+var url = "http://web.ics.purdue.edu/~gchopra/class/public/pages/webdesign/05_simple.html";
 //var url = "https://www.google.com/";
 //var url = "https://curl.haxx.se/libcurl/c/https.html"
 //var url = "https://www.w3schools.com/"
 
 DKDuktape_Create("DKRocket");
 window.location.href = url;
+
+//test location
+console.warn("hash: "+window.location.hash);
+console.warn("host: "+window.location.host);
+console.warn("hostname: "+window.location.hostname);
+console.warn("href: "+window.location.href);
+console.warn("origin: "+window.location.origin);
+console.warn("password: "+window.location.password);
+console.warn("pathname: "+window.location.pathname);
+console.warn("port: "+window.location.port);
+console.warn("protocol: "+window.location.protocol);
+console.warn("search: "+window.location.search);
+console.warn("username: "+window.location.username);
+
 
 DKDuktape_Create("DKCef");
 DKCef_NewBrowser("CefPopup", 0, 0, 416, 638, url);
