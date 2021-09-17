44348ebea1af17c5a41f1c7f3af7911683934388
aquawicket <aquawicket@hotmail.com>
2021-09-17 14:45:57 -0700

    commit from git

--- DKApps/DigitalKnob/assets/app.js
+++ DKApps/DigitalKnob/assets/app.js
@@ -2,7 +2,7 @@ var USE_CEF     = 1; //Desktop
 var USE_WEBVIEW = 0; //TODO: Android, iOS
 var USE_SDL     = 0; //Use with caution
 var USE_RML  = 1; //Use with caution
-var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"/index.html";
+var DKApp_url   = "file:///"+CPP_DKAssets_LocalAssets()+"/index.html";
 //var DKApp_url = "http://digitalknob.com/Digitalknob/";
 
 CPP_DK_Create("DK/init.js", function(){});
@@ -14,14 +14,17 @@ function app_LoadPlugins()
 
 	CPP_DK_Create("DKGoogleAd/DKGoogleAd.js", function(){
 		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
-		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
+		if(id)
+			document.getElementById(id).style["bottom"] = 0px;
 	});
 }
 
 ///////////////////////
 function app_LoadPage()
 {
-	DKWidget_SetProperty("body","background-color","grey");
+	//DKWidget_SetProperty("body","background-color","grey");
+	document.body.style["background-color"] = "grey";
+	
 	CPP_DK_Create("DKWindow/DKWindow.js", function(){
 	CPP_DK_Create("DKScale/DKScale.js", function(){
 	CPP_DK_Create("DKGui/DKFrame.js", function(){
@@ -29,7 +32,8 @@ function app_LoadPage()
 	CPP_DK_Create("Digitalknob/Digitalknob.js", function(){});
 	CPP_DK_Create("DKGoogleAd/DKGoogleAd.js", function(){
 		var id = DKGoogleAd_CreateAd("body", "100%", "100rem");
-		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
+		if(id)
+			document.getElementById(id).style["bottom"] = 0px;
 	});
 	});
 	});
