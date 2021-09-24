9aa57479491009399ac24bd88aab99ded08ca304
aquawicket <aquawicket@hotmail.com>
2021-09-24 04:53:07 -0700

    commit from git

--- DKPlugins/SchultzEvolution/Panel3.js
+++ DKPlugins/SchultzEvolution/Panel3.js
@@ -1,21 +1,17 @@
 var current_record = 0;
 
-//////////////////////
-function Panel3_Init()
-{
+function Panel3_Init(){
 	CPP_DK_Create("DKMySql.js");
 	CPP_DK_Create("SchultzEvolution/Panel3.html");
 
-	DKMySql_SetPhp("http://digitalknob.com/digitalknob.com");
-	DKMySql_Connect("mysql.schultzevolution.com", "aquawicket", "8BallBreak", "");
+	DKMySql_SetPhp("FIXME.com");
+	DKMySql_Connect("mysql.schultzevolution.com", "name", "pass", "");
 	DKMySql_Database("schultzevolution");
 	
 	current_record = DKWidgetMySql_GetFirstRecordNum("Panel3.html");
 	DKWidgetMySql_LoadRecord("Panel3.html", current_record);
 }
 
-//////////////////////////////
-function Panel3_OnEvent(event)
-{
+function Panel3_OnEvent(event){
 	DKLog("Panel3_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n");
 }
