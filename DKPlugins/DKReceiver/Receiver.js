2c70bd99f764fdb06fffa4082b07910f542521c1
aquawicket <aquawicket@hotmail.com>
2023-02-13 16:07:02 -0500

    commit from git

--- DKPlugins/DKReceiver/Receiver.js
+++ DKPlugins/DKReceiver/Receiver.js
@@ -28,7 +28,7 @@ function Receiver_init(){
 		server.addEventListener("message", function onmessage(event){
 			console.log("server.onmessage("+event+")");
 			console.log("event.value = "+event.value);
-			//byId("DKUWebSocketsServer_receive").value = event.value;
+			Receiver_onmessage(event.value);
 		})
 
 		//server.onopen = function onopen(event){
@@ -40,8 +40,12 @@ function Receiver_init(){
 		server.start(local_ip, "80");
 	
 		byId("IPAddress").innerHTML = local_ip
-		byId("VolumeUp_Button").addEventListener("click", Receiver_onevent);
-		byId("VolumeDown_Button").addEventListener("click", Receiver_onevent);
+		byId("VolumeUp_Button").addEventListener("click", function volume_up(event){
+			Receiver_onevent(event)
+		});
+		byId("VolumeDown_Button").addEventListener("click", function volume_down(event){
+			Receiver_onevent(event)
+		});
 	});
 }
 
@@ -54,56 +58,52 @@ function Receiver_End(){
 function Receiver_onevent(event){
 	//DKDEBUGFUNC(event);	
 	console.log("Receiver_onevent("+event+")")
-	/*
+	
 	if(event.currentTarget.id === "VolumeUp_Button"){
-		console.log("Button: VolumeUp\n");
 		var volume = CPP_DK_GetVolume();
-		if(DK_GetOS() == "Linux"){
-			CPP_DK_SetVolume(volume+5000);
-		}
-		else{
+		//if(DK_GetOS() == "Linux"){
+		//	CPP_DK_SetVolume(volume+5000);
+		//}
+		//else{
 			CPP_DK_SetVolume(volume+2); // 1 doesn't work.   debug me
-		}
+		//}
 	}
 	if(event.currentTarget.id === "VolumeDown_Button"){
-		console.log("Button: Volume Down\n");
 		var volume = CPP_DK_GetVolume();
-		if(DK_GetOS() == "Linux"){
-			CPP_DK_SetVolume(volume-5000);
-		}
-		else{
+		//if(DK_GetOS() == "Linux"){
+		//	CPP_DK_SetVolume(volume-5000);
+		//}
+		//else{
 			CPP_DK_SetVolume(volume-2);
-		}
+		//}
 	}
-	*/
-	/*
-	if(event.type === "DKWebSockets_OnMessageFromClient"){
-		console.log("server: "+DK_GetValue(event)+"\n");
-		if(DK_GetValue(event) == "Power"){
-			console.log("Client: Power\n");
-		}
-		if(DK_GetValue(event) == "VolumeUp"){
-			console.log("Client: VolumeUp\n");
-			var volume = DK_GetVolume();
-			if(DK_GetOS() != "Linux"){
-				DK_SetVolume(volume+2);
-				DKTray_ShowBalloon("Volume Up");
-			}
-			else{
-				DK_SetVolume(volume+5000);
-			}
-		}
-		if(DK_GetValue(event) == "VolumeDown"){
-			console.log("Client: VolumeDown\n");
-			var volume = DK_GetVolume();
-			if(DK_GetOS() != "Linux"){
-				DK_SetVolume(volume-2);
-				DKTray_ShowBalloon("Volume Down");
-			}
-			else{
-				DK_SetVolume(volume-5000);
-			}
-		}
+}
+
+function Receiver_onmessage(message){
+	console.log("Receiver_onmessage("+message+")");
+	if(message == "Power"){
+		console.log("Client: Power\n");
 	}
-	*/
+	if(message == "VolumeUp"){
+		console.log("Client: VolumeUp\n");
+		var volume = CPP_DK_GetVolume();
+		//if(DK_GetOS() != "Linux"){
+			CPP_DK_SetVolume(volume+2); // 1 doesn't work.   debug me
+			//DKTray_ShowBalloon("Volume Up");
+		//}
+		//else{
+		//	DK_SetVolume(volume+5000);
+		//}
+	}
+	if(message == "VolumeDown"){
+		console.log("Client: VolumeDown\n");
+		var volume = CPP_DK_GetVolume();
+		//if(DK_GetOS() != "Linux"){
+			CPP_DK_SetVolume(volume-2);
+			//DKTray_ShowBalloon("Volume Down");
+		//}
+		//else{
+		//	DK_SetVolume(volume-5000);
+		//}
+	}
 }
