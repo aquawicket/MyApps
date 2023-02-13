341b6820baff5c0b2a6acf7760c01de4a88280fa
aquawicket <aquawicket@hotmail.com>
2023-02-12 22:05:53 -0500

    commit from git

--- DKApps/DKReceiver/assets/myapp.js
+++ DKApps/DKReceiver/assets/myapp.js
@@ -4,11 +4,12 @@ CPP_DK_Create("DKRml");
 //var url = CPP_DKAssets_LocalAssets()+"/index.html";
 //location.href = url
 
-//CPP_DK_Create("DKScale/DKScale.js", function(){});
-CPP_DK_Create("DKReceiver/Receiver.js", function(){});
-CPP_DK_Create("DKTray/DKTray.js", function(){});
-CPP_DK_Create("DKDebug/DKDebug.js", function(){});
+//DKPlugin("DKScale/DKScale.js", function(){});
+DKPlugin("DKReceiver/Receiver.js", function(){});
+DKPlugin("DKTray/DKTray.js", function(){});
+DKPlugin("DKDebug/DKDebug.js", function(){});
 
+/*
 DKAddEvent("GLOBAL", "minimize", app_OnEvent);
 
 ///////////////////////////
@@ -18,3 +19,4 @@ function app_OnEvent(event)
 	CPP_DK_Create("DKWindowJS");
 	CPP_DKWindow_Hide();
 }
+*/
