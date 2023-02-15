6a294e23b275e2f2277c4d2fa1d46069b10dd9c5
aquawicket <aquawicket@hotmail.com>
2023-02-15 18:55:21 -0500

    commit from git

--- DKApps/DKScreenRecorder/assets/myapp.js
+++ DKApps/DKScreenRecorder/assets/myapp.js
@@ -1,18 +1,10 @@
-var USE_CEF     = 0; //Desktop
-var USE_WEBVIEW = 0; //TODO: Android, iOS
-var USE_SDL     = 0; //Use with caution
-var USE_RML  = 0; //Use with caution
-var DKApp_url   = "file:///"+CPP_DKAssets_LocalAssets()+"index.html";
-
-//CPP_DK_Create("DK/init.js", function(){});
-
-function app_OnEvent(event){
+function app_onevent(event){
 	//DKDEBUGFUNC(event);
-	if(DK_Type(event, "1003")){ //Tray, Fullscreen
+	if(event.type === "1003"){ //Tray, Fullscreen
 		console.log("Clicked Tray -> Record\n");
 		DKScreenRecorder_Record("video.avi"); //Record the screen to a file.
 	}
-	if(DK_Type(event, "1004")){ //Tray, Fullscreen
+	if(event.type === "1004"){ //Tray, Fullscreen
 		console.log("Clicked Tray -> Stop\n");
 		DKScreenRecorder_Stop();
 	}
@@ -21,10 +13,11 @@ function app_OnEvent(event){
 function app_LoadPlugins(){
 	DKPlugin("DKScreenRecorder");
 	DKPlugin("DKTray/DKTray.js", function(){
+		DKTray_init()
 		DKTray_AddItem("Record", 1003);
-		DKAddEvent("DKTray", "1003", app_OnEvent);
+		DKAddEvent("DKTray", "1003", app_onevent);
 		DKTray_AddItem("Stop", 1004);
-		DKAddEvent("DKTray", "1004", app_OnEvent);
+		DKAddEvent("DKTray", "1004", app_onevent);
 	});
 }
 
