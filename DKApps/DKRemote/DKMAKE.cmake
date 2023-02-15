101cd9c0d7a8d623fe9806192620ffe7798d844f
aquawicket <aquawicket@hotmail.com>
2023-02-15 18:33:48 -0500

    commit from git

--- DKApps/DKRemote/DKMAKE.cmake
+++ DKApps/DKRemote/DKMAKE.cmake
@@ -1,7 +1,6 @@
 dk_depend(DK)
 dk_depend(DKDuktape)
 dk_depend(DKArchive)
-dk_depend(DKAssets)
 dk_depend(DKDebug)
 dk_depend(DKDom)
 dk_depend(DKFile)
--- DKApps/DKScreenRecorder/DKMAKE.cmake
+++ DKApps/DKScreenRecorder/DKMAKE.cmake
@@ -1,9 +1,7 @@
 ## http://tommy.chheng.com/post/123568080616/encode-opengl-to-video-with-opencv
-dk_depend(DK)
-dk_depend(DKDuktape)
+dk_depend(DKDom)
 dk_depend(DKAssets)
 dk_depend(DKFile)
 dk_depend(DKScreenRecorder)
-#dk_depend(DKSDLRml)
-dk_depend(DKFile)
+dk_depend(DKSDLRml)
 dk_depend(DKTray)
