e05ce887d3260fe6f0f5292e6fb9dd50d68165ac
aquawicket <aquawicket@hotmail.com>
2023-02-15 19:19:24 -0500

    commit from git

--- DKApps/DKTray/assets/Tray.js
+++ DKApps/DKTray/assets/Tray.js
@@ -1,13 +1,10 @@
-/////////////////////
-function Tray_Init()
-{
-	
+function Tray_Init(){
 	CPP_DK_Create("DKTray");
-	DKAddEvent("DKTray", "1000", Tray_OnEvent);
-	DKAddEvent("DKTray", "1001", Tray_OnEvent);
-	DKAddEvent("DKTray", "1002", Tray_OnEvent);
-	DKAddEvent("DKTray", "click", Tray_OnEvent);
-	DKAddEvent("DKTray", "doubleclick", Tray_OnEvent);
+	DKAddEvent("DKTray", "1000", Tray_onevent);
+	DKAddEvent("DKTray", "1001", Tray_onevent);
+	DKAddEvent("DKTray", "1002", Tray_onevent);
+	DKAddEvent("DKTray", "click", Tray_onevent);
+	DKAddEvent("DKTray", "doubleclick", Tray_onevent);
 	
 	DKTray_AddItem("Exit", 1002);
 	DKTray_AddItem("Minimize", 1001);
@@ -20,58 +17,44 @@ function Tray_Init()
 	DKTray_ShowBalloon("DKTray");
 }
 
-////////////////////////////
-function Tray_OnEvent(event)
-{
-	DKDEBUGFUNC(event);
-	if(DK_Type(event, "1000") || DK_Type(event, "doubleclick")){
+function Tray_onevent(event){
+	//DKDEBUGFUNC(event);
+	if(event.type === "1000" || event.type === "doubleclick"){
 		CPP_DK_Create("DKWindowJS");
 		CPP_DKWindow_Show();
 		DKTray_ShowBalloon("DKTray restored");
 	}
-	if(DK_Type(event, "1001")){
+	if(event.type === "1001"){
 		CPP_DK_Create("DKWindowJS");
 		CPP_DKWindow_Hide();
 		DKTray_ShowBalloon("DKTray minimized");
 	}
-	if(DK_Type(event, "1002")){
+	if(event.type === "1002"){
 		DK_Exit();
 	}
 	
-	if(DK_Type(event, "click")){
+	if(event.type === "click"){
 		Tray_ToggleIcon();
 		DKTray_ShowBalloon("DKTray clicked");
 	}
 }
 
-//////////////////////////
-function Tray_ToggleIcon()
-{
-	
+function Tray_ToggleIcon(){
 	var icon = DKTray_GetIcon();
 	var file = DKFile_GetFilename(icon);
 	console.log("current icon = "+file+"\n");
-	
-	if(file == "touchON.ico"){
+	if(file == "touchON.ico")
 		Tray_Off();
-	}
-	else{
+	else
 		Tray_On();
-	}
 }
 
-//////////////////
-function Tray_On()
-{
-	
+function Tray_On(){
 	var assets = DKAssets_LocalAssets();
 	DKTray_SetIcon(assets+"touchON.ico");
 }
 
-///////////////////
-function Tray_Off()
-{
-	
+function Tray_Off(){
 	var assets = DKAssets_LocalAssets();
 	DKTray_SetIcon(assets+"touchOFF.ico");
 }
