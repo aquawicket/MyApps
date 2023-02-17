1ea2c8a720f60b3f99df7a5d8bb87b0f411ddcc5
aquawicket <aquawicket@hotmail.com>
2023-02-16 19:04:23 -0500

    commit from git

--- DKApps/DKTray/assets/app.js
+++ DKApps/DKTray/assets/app.js
@@ -1,3 +1,3 @@
-CPP_DK_Create("DKDebug/DKDebug.js")
-CPP_DK_Create("Tray.js");
-Tray_init()
+CPP_DK_Create("DKSDLWindow")
+CPP_DK_Create("DKRml")
+location.href = CPP_DKAssets_LocalAssets()+"index.html"
