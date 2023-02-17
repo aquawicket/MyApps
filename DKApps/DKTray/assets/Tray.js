c906c773facb841293eb52fdc86c70c8be1f43c2
aquawicket <aquawicket@hotmail.com>
2023-02-16 21:59:28 -0500

    commit from git

--- DKApps/DKTray/assets/Tray.js
+++ DKApps/DKTray/assets/Tray.js
@@ -1,20 +1,23 @@
+var tray
+
 function Tray_init(){
-	CPP_DK_Create("DKTray");
-	window.addEventListener("1000", Tray_onevent);
-	window.addEventListener("1001", Tray_onevent);
-	window.addEventListener("1002", Tray_onevent);
-	window.addEventListener("click", Tray_onevent);
-	window.addEventListener("doubleclick", Tray_onevent);
+	dk.create("DKTray");
+	tray = new Tray();
+	tray.addEventListener("1000", Tray_onevent);
+	tray.addEventListener("1001", Tray_onevent);
+	tray.addEventListener("1002", Tray_onevent);
+	tray.addEventListener("click", Tray_onevent);
+	tray.addEventListener("doubleclick", Tray_onevent);
 	
-	CPP_DKTray_AddItem("Exit", 1002);
-	CPP_DKTray_AddItem("Minimize", 1001);
-	CPP_DKTray_AddItem("Restore", 1000);
+	tray.addItem("Exit", 1002);
+	tray.addItem("Minimize", 1001);
+	tray.addItem("Restore", 1000);
 	
-	CPP_DKTray_SetTooltip("DKTray example");
+	tray.setTooltip("DKTray example");
 	
 	Tray_On();
 	
-	CPP_DKTray_ShowBalloon("DKTray");
+	tray.showBalloon("DKTray");
 }
 
 function Tray_onevent(event){
@@ -22,12 +25,12 @@ function Tray_onevent(event){
 	if(event.type === "1000" || event.type === "doubleclick"){
 		CPP_DK_Create("DKWindowJS");
 		CPP_DKWindow_Show();
-		DKTray_ShowBalloon("DKTray restored");
+		tray.showBalloon("DKTray restored");
 	}
 	if(event.type === "1001"){
 		CPP_DK_Create("DKWindowJS");
 		CPP_DKWindow_Hide();
-		DKTray_ShowBalloon("DKTray minimized");
+		tray.showBalloon("DKTray minimized");
 	}
 	if(event.type === "1002"){
 		DK_Exit();
@@ -35,7 +38,7 @@ function Tray_onevent(event){
 	
 	if(event.type === "click"){
 		Tray_ToggleIcon();
-		DKTray_ShowBalloon("DKTray clicked");
+		tray.showBalloon("DKTray clicked");
 	}
 }
 
@@ -51,10 +54,10 @@ function Tray_ToggleIcon(){
 
 function Tray_On(){
 	var assets = CPP_DKAssets_LocalAssets();
-	CPP_DKTray_SetIcon(assets+"touchON.ico");
+	tray.setIcon(assets+"touchON.ico");
 }
 
 function Tray_Off(){
 	var assets = CPP_DKAssets_LocalAssets();
-	CPP_DKTray_SetIcon(assets+"touchOFF.ico");
+	tray.setIcon(assets+"touchOFF.ico");
 }
