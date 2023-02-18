9b57db651f650374c4e4782ba94429d979b29c97
aquawicket <aquawicket@hotmail.com>
2023-02-18 14:23:04 -0500

    commit from git

--- DKApps/DKOcr/DKMAKE.cmake
+++ DKApps/DKOcr/DKMAKE.cmake
@@ -1,8 +1,5 @@
-dk_enable(HAVE_DKCef) ##FIXME: Find a way to remove this requirement
-
 dk_depend(DK)
 dk_depend(DKDuktape)
-dk_depend(DKCef)
 dk_depend(DKDebug)
 dk_depend(DKDom)
 dk_depend(DKFile)
