e6194b4d65df1d1c28d816615ef06f9944058bae
aquawicket <aquawicket@hotmail.com>
2023-02-19 15:09:01 -0500

    commit from git

--- DKApps/DKEnvelope/assets/app.js
+++ DKApps/DKEnvelope/assets/app.js
@@ -1,39 +1,5 @@
-var USE_CEF     = 1; //Desktop
-var USE_WEBVIEW = 0; //TODO: Android, iOS
-var USE_SDL     = 0; //Use with caution
-var USE_RML  = 1; //Use with caution
-var DKApp_url   = "file:///"+DKAssets_LocalAssets()+"/index.html";
-//var DKApp_url = "http://TODO.com/DKEnvelope"
-
-
-CPP_DK_Create("DK/init.js", function(){});
-
-//////////////////////////
-function app_LoadPlugins()
-{
-	
-	CPP_DK_Create("DKDebug/DKDebug.js", function(){});
-	CPP_DK_Create("DKGoogleAd/DKGoogleAd.js", function(){
-		var id = DKGoogleAd_CreateAd("body", "100%", "90rem");
-		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
-	});
-}
-
-///////////////////////
-function app_LoadPage()
-{
-	
-	DKWidget_SetProperty("body","background-color","grey");
-	CPP_DK_Create("DKWindow/DKWindow.js", function(){
-	CPP_DK_Create("DKScale/DKScale.js", function(){
-	CPP_DK_Create("DKDebug/DKDebug.js", function(){
-	CPP_DK_Create("DKEnvelope/DKEnvelope.js", function(){
-	CPP_DK_Create("DKGoogleAd/DKGoogleAd.js", function(){
-		var id = DKGoogleAd_CreateAd("body", "100%", "90rem");
-		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
-	});
-	});
-	});
-	});
-	});
-}
+CPP_DK_Create("DKSDLWindow")
+CPP_DK_Create("DKRml")
+location.href = CPP_DKAssets_LocalAssets()+"index.html"
+myapp.loadFiles()
+myapp.loadApp()
---
+++ DKApps/DKEnvelope/assets/myapp.js
@@ -0,0 +1,27 @@
+function MyApp(){}
+const myapp = DKPlugin(MyApp);
+
+
+myapp.loadFiles = function myapp_loadFiles() {
+	DKPlugin("DKDebug/DKDebug.js")
+	DKPlugin("DKGoogleAd/DKGoogleAd.js", function(){
+		var id = DKGoogleAd_CreateAd("body", "100%", "90rem");
+		if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
+	});
+}
+
+myapp.loadApp = function myapp_loadApp() {
+	document.body.style.backgroundColor = "grey"
+	DKPlugin("DKWindow/DKWindow.js", function(){
+		DKPlugin("DKScale/DKScale.js", function(){
+			DKPlugin("DKDebug/DKDebug.js", function(){
+				DKPlugin("DKEnvelope/DKEnvelope.js", function(){
+					DKPlugin("DKGoogleAd/DKGoogleAd.js", function(){
+						var id = DKGoogleAd_CreateAd("body", "100%", "90rem");
+						if(id){ DKWidget_SetProperty(id, "bottom", "0px"); }
+					});
+				});
+			});
+		});
+	});
+}
