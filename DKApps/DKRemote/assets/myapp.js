4f0d39c82a6044fc633ede2b6aaa2e59e575784a
aquawicket <aquawicket@hotmail.com>
2023-02-20 18:28:04 -0500

    commit from git

--- DKApps/DKRemote/assets/myapp.js
+++ DKApps/DKRemote/assets/myapp.js
@@ -2,14 +2,14 @@ function MyApp(){}
 const myapp = DKPlugin(MyApp);
 
 myapp.loadFiles = function myapp_loadFiles() {
-	console.log("!!!! myapp.loadFiles !!!!")
+	console.log("\n!!!! myapp.loadFiles !!!!")
 	DKPlugin("DK/DKPhp.js")
 	DKPlugin("DKFile/DKFile.js")
 	DKPlugin("DKRemote/Remote.js")
 }
 
 myapp.loadApp = function myapp_loadApp() {
-	console.log("!!!! myapp.loadApp !!!!")
+	console.log("\n!!!! myapp.loadApp !!!!")
 	Remote_init()
 }
 
