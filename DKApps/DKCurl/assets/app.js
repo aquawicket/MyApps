508bc29839cf6d7c359c8fd9e4fcf7650dc912b5
aquawicket <aquawicket@hotmail.com>
2023-02-20 14:56:06 -0500

    commit from git

--- DKApps/DKCurl/assets/app.js
+++ DKApps/DKCurl/assets/app.js
@@ -1,11 +1,11 @@
 CPP_DK_Create("DKCurl");
 
 //TODO
-var str = DK_GetArgs(); //Get arguments
+var str = CPP_DK_GetArgs(); //Get arguments
 //console.log("args = "+str+"\n");
 var args = str.split(";");
 if(args.length < 2){
 	DKERROR("Incorrect number of arguments\n");
 }
-DKCurl_Download(args[0], args[1]);
-DK_Exit();
+CPP_DKCurl_Download(args[0], args[1]);
+CPP_DK_Exit();
